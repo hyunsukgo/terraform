a0020257d06741c91293aab455e8f9b19159567f
Administrator <hyunseokjung163@gmail.com>
2021-01-05 14:48:15 +0900

    Add VPCs

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,36 @@
+resource "aws_vpc" "ski-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "ski-vpc"
+  }
+}
+resource "aws_vpc" "skens-vpc" {
+  cidr_block       = "10.1.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "skens-vpc"
+  }
+}
 resource "aws_vpc" "skhc-vpc" {
-  cidr_block       = "10.0.0.0/24"
+  cidr_block       = "10.2.0.0/24"
   instance_tenancy = "default"
   enable_dns_hostnames = "true"
 
   tags = {
     Name = "skhc-vpc"
+  }
+}
+resource "aws_vpc" "supex-vpc" {
+  cidr_block       = "10.3.0.0/24"
+  instance_tenancy = "default"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "supex-vpc"
   }
 }
