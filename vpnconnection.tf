a245f3a9bba2c976a9f0537616ecffb03a5bb1e0
Administrator <hyunseokjung163@gmail.com>
2021-01-07 11:47:01 +0900

    Add Dependency

--- vpnconnection.tf
+++ vpnconnection.tf
@@ -4,6 +4,7 @@ resource "aws_vpn_connection" "ski-to-vdi" {
   customer_gateway_id = ski-cgw.customer_gateway.id
   type                = "ipsec.1"
   static_routes_only  = true
+  depends_on = [aws_vpn_gateway.ski-vpg, customer_gateway.ski-cgw]
 }
 ## SK 이노베이션과 VDI망 연결 ##
 resource "aws_vpn_connection" "skens-to-vdi" {
@@ -11,6 +12,7 @@ resource "aws_vpn_connection" "skens-to-vdi" {
   customer_gateway_id = skens-cgw.customer_gateway.id
   type                = "ipsec.1"
   static_routes_only  = true
+  depends_on = [aws_vpn_gateway.skens-vpg, customer_gateway.skens-cgw]
 }
 ## SK 이노베이션과 VDI망 연결 ##
 resource "aws_vpn_connection" "skhc-to-vdi" {
@@ -18,6 +20,7 @@ resource "aws_vpn_connection" "skhc-to-vdi" {
   customer_gateway_id = skhc-cgw.customer_gateway.id
   type                = "ipsec.1"
   static_routes_only  = true
+  depends_on = [aws_vpn_gateway.skhc-vpg, customer_gateway.skhc-cgw]
 }
 ## SK 이노베이션과 VDI망 연결 ##
 resource "aws_vpn_connection" "supex-to-vdi" {
@@ -25,6 +28,7 @@ resource "aws_vpn_connection" "supex-to-vdi" {
   customer_gateway_id = supex-cgw.customer_gateway.id
   type                = "ipsec.1"
   static_routes_only  = true
+  depends_on = [aws_vpn_gateway.supex-vpg, customer_gateway.supex-cgw]
 }
 ## SK 이노베이션과 VDI망 연결 ##
 resource "aws_vpn_connection" "skhc-to-datalake" {
@@ -32,6 +36,7 @@ resource "aws_vpn_connection" "skhc-to-datalake" {
   customer_gateway_id = skhc-cgw.customer_gateway.id
   type                = "ipsec.1"
   static_routes_only  = true
+  depends_on = [aws_vpn_gateway.datalake-vpg, customer_gateway.skhc-cgw]
 }
 ## SK 이노베이션과 VDI망 연결 ##
 resource "aws_vpn_connection" "ski-to-datalake" {
@@ -39,12 +44,13 @@ resource "aws_vpn_connection" "ski-to-datalake" {
   customer_gateway_id = ski-cgw.customer_gateway.id
   type                = "ipsec.1"
   static_routes_only  = true
+  depends_on = [aws_vpn_gateway.datalake-vpg, customer_gateway.ski-cgw]
 }
-/*## SK 이노베이션과 VDI망 연결 ##
+## SK 이노베이션과 VDI망 연결 ##
 resource "aws_vpn_connection" "skens-to-datalake" {
   vpn_gateway_id      = datalake-vpg.vpn_gateway.id
   customer_gateway_id = skens-cgw.customer_gateway.id
   type                = "ipsec.1"
   static_routes_only  = true
+  depends_on = [aws_vpn_gateway.datalake-vpg, customer_gateway.skens-cgw]
 }
-*/
