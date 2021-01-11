40a51fdced30665a19b2242e4e2fbe6157c5b32b
Administrator <hyunseokjung163@gmail.com>
2021-01-11 14:42:13 +0900

    Removce Redshift

--- aws_redshift_cluster.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_redshift_cluster" "default" {
-  cluster_identifier = "tf-redshift-cluster"
-  database_name      = "mydb"
-  master_username    = "foo"
-  master_password    = "Mustbe8characters"
-  node_type          = "dc1.large"
-  cluster_type       = "single-node"
-}
