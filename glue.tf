92145607a9e0a840c44fb997ad61484d0f45b932
Administrator <hyunseokjung163@gmail.com>
2021-01-12 11:02:46 +0900

    Remove Glue

--- glue.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
-  name = "MyCatalogDatabase"
-}
-resource "aws_glue_catalog_table" "aws_glue_catalog_table" {
-  name          = "MyCatalogTable"
-  database_name = "MyCatalogDatabase"
-}
