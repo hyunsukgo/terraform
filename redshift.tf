3dd368bfc6840b3559954e1dd97fd0ed7e199882
Administrator <hyunseokjung163@gmail.com>
2021-01-12 14:00:03 +0900

    Update redshift.tf
    final_snapshot_identifier
    설정 추가
    subnet 그룹 이름 변경

--- redshift.tf
+++ redshift.tf
@@ -12,7 +12,7 @@ module "redshift" {
 
   # Group parameters
   wlm_json_configuration = "[{\"query_concurrency\": 5}]"
-
+  final_snapshot_identifier = true
   # DB Subnet Group Inputs
   subnets = [aws_redshift_subnet_group.ski-redshift-subnet-group.id]
   vpc_security_group_ids =[aws_security_group.ski-redshift-sg.id]
@@ -20,9 +20,8 @@ module "redshift" {
   cluster_iam_roles = ["arn:aws:iam::225367859851:role/glue_service_role"]
 }
 resource "aws_redshift_subnet_group" "ski-redshift-subnet-group" {
-  name       = "foo"
+  name       = "ski-redshift-subnet-group"
   subnet_ids = [aws_subnet.ski-redshift-subnet-a.id, aws_subnet.ski-redshift-subnet-c.id]
-
   tags = {
     environment = "Production"
     name = "ski-redshift-subnet-group"
