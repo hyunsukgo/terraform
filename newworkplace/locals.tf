db87078bce90c5a19969032784ad677773c8658e
Administrator <hyunseokjung163@gmail.com>
2021-01-13 15:34:23 +0900

    Local
    String Triming Test

--- newworkplace/locals.tf
+++ newworkplace/locals.tf
@@ -2,4 +2,5 @@ locals {
   service_name = "skcc"
   owner        = "Community Team"
   cidr = "10.0.0.0/16"
+  cidrtrim = substr("local.cidr", 0, 3)
 }
--- newworkplace/subnets.tf
+++ newworkplace/subnets.tf
@@ -1,24 +1,24 @@
-resource "aws_subnet" "${local.service_name}-redshift-subnet-a" {
+resource "aws_subnet" "redshift-subnet-a" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/25"
+  cidr_block = "${substr("local.cidr", 0, 3)}.1.0/25"
   availability_zone = "ap-northeast-2a"
   tags = {
-    Name = "ski-redshift-subnet-a"
+    Name = "${local.service_name}-redshift-subnet-a"
   }
 }
-resource "aws_subnet" "${local.service_name}-redshift-subnet-c" {
+resource "aws_subnet" "redshift-subnet-c" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.1.128/25"
+  cidr_block = "${substr("local.cidr", 0, 3)}.1.128/25"
   availability_zone = "ap-northeast-2c"
   tags = {
-    Name = "ski-redshift-subnet-c"
+    Name = "${local.service_name}-redshift-subnet-c"
   }
 }
-resource "aws_subnet" "${local.service_name}-glue-subnet" {
+resource "aws_subnet" "glue-subnet" {
   vpc_id     = aws_vpc.ski-vpc.id
-  cidr_block = "10.0.100.0/24"
+  cidr_block = "${substr("local.cidr", 0, 3)}.100.0/24"
 
   tags = {
-    Name = "ski-glue-subnet"
+    Name = "${local.service_name}-glue-subnet"
   }
 }
