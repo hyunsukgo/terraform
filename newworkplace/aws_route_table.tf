90696f3b81fa5a9e51a9253ee7bd5e467f34fe5b
Administrator <hyunseokjung163@gmail.com>
2021-01-13 17:16:08 +0900

    depends_on 추가
    depends_on

--- newworkplace/aws_route_table.tf
+++ newworkplace/aws_route_table.tf
@@ -1,5 +1,5 @@
-resource "aws_route_table" "route-table" {
-
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "10.0.1.0/24"
     gateway_id = aws_vpn_gateway.vpg.id
--- newworkplace/vpn_connection.tf
+++ newworkplace/vpn_connection.tf
@@ -11,4 +11,5 @@ resource "aws_vpn_connection" "customer-to-vdi" {
 resource "aws_vpn_gateway_route_propagation" "route_propagation" {
   vpn_gateway_id = aws_vpn_gateway.vpg.id
   route_table_id = aws_route_table.route_table.id
+  depends_on = [aws_route_table.route_table,aws_vpn_gateway.vpg]
 }
