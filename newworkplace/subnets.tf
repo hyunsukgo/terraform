aa3bfefb547834922b61d529d69cdd157d6c8558
Administrator <hyunseokjung163@gmail.com>
2021-01-13 15:38:55 +0900

    Update subnets.tf
    Error 수정

--- newworkplace/subnets.tf
+++ newworkplace/subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "redshift-subnet-a" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "${substr("${local.cidr}", 0, 3)}.1.0/25"
+  cidr_block = "${substr("${local.cidr}", 0, 4)}.1.0/25"
   availability_zone = "ap-northeast-2a"
   tags = {
     Name = "${local.service_name}-redshift-subnet-a"
@@ -8,15 +8,15 @@ resource "aws_subnet" "redshift-subnet-a" {
 }
 resource "aws_subnet" "redshift-subnet-c" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "${substr("${local.cidr}", 0, 3)}.1.128/25"
+  cidr_block = "${substr("${local.cidr}", 0, 4)}.1.128/25"
   availability_zone = "ap-northeast-2c"
   tags = {
     Name = "${local.service_name}-redshift-subnet-c"
   }
 }
 resource "aws_subnet" "glue-subnet" {
-  vpc_id     = aws_vpc.ski-vpc.id
-  cidr_block = "${substr("${local.cidr}", 0, 3)}.100.0/24"
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${substr("${local.cidr}", 0, 4)}.100.0/24"
 
   tags = {
     Name = "${local.service_name}-glue-subnet"
