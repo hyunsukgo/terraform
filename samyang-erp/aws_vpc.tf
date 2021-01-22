7a63c35f1f28478665c8611942588b7389013997
Administrator <hyunseokjung163@gmail.com>
2021-01-22 10:44:48 +0900

    재사용성 강화

--- samyang-erp/aws_vpc.tf
+++ samyang-erp/aws_vpc.tf
@@ -1,9 +1,8 @@
 resource "aws_vpc" "samyang_vpc" {
-    cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_hostnames = "true"
-
-  tags = {
-    Name = "samyang-vpc"
+    cidr_block       = "${substr("${local.cidr}"
+    instance_tenancy = "default"
+    enable_dns_hostnames = "true"
+    tags = {
+        Name = "samyang-vpc"
   }
 }
