27180ba323afa71a1ab1d03cc8bfbd1ce37b2121
Administrator <hyunseokjung163@gmail.com>
2021-01-27 12:43:06 +0900

    Create aws_subnet.tf
    서브넷 추가

---
+++ argocd/aws_subnet.tf
@@ -0,0 +1,16 @@
+resource "aws_subnet" "eks-subnet-a" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${substr("${local.cidr}", 0, 4)}.1.0/25"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "${local.service_name}-redshift-subnet-a"
+  }
+}
+resource "aws_subnet" "eks-subnet-c" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${substr("${local.cidr}", 0, 4)}.1.128/25"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    Name = "${local.service_name}-redshift-subnet-c"
+  }
+}
