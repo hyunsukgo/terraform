63c1a05939f575052e40083a8a85ace44f5e893f
Administrator <hyunseokjung163@gmail.com>
2021-01-28 13:21:18 +0900

    Update aws_subnet.tf
    서브넷 할당을 위한 내장 함수 검토 필요~!

--- argocd/aws_subnet.tf
+++ argocd/aws_subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "eks-subnet-a" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "${substr("${local.cidr}", 0, 4)}.1.0/25"
+  cidr_block = "${substr("${local.cidr}", 0, 6)}.1.0/25"
   availability_zone = "ap-northeast-2a"
   tags = {
     Name = "${local.service_name}-redshift-subnet-a"
@@ -8,7 +8,7 @@ resource "aws_subnet" "eks-subnet-a" {
 }
 resource "aws_subnet" "eks-subnet-c" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "${substr("${local.cidr}", 0, 4)}.1.128/25"
+  cidr_block = "${substr("${local.cidr}", 0, 6)}.1.128/25"
   availability_zone = "ap-northeast-2c"
   tags = {
     Name = "${local.service_name}-redshift-subnet-c"
