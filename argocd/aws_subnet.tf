4bd387ce07dcbe83e1fd8b9ee1c0c5d2058bd3e3
Administrator <hyunseokjung163@gmail.com>
2021-01-29 16:00:44 +0900

    Update aws_subnet.tf
    EKS 전용 서브넷 추가

--- argocd/aws_subnet.tf
+++ argocd/aws_subnet.tf
@@ -14,3 +14,17 @@ resource "aws_subnet" "eks-subnet-c" {
     Name = "${local.service_name}-subnet-c"
   }
 }
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+resource "aws_subnet" "eks-nodes-group" {
+  count = 2
+
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
+  vpc_id            = aws_vpc.vpc.id
+
+  tags = {
+    "kubernetes.io/cluster/${aws_eks_cluster.example.name}" = "shared"
+  }
+}
