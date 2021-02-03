098d4ac284ccd93a192670200937fa57a0219fea
Administrator <hyunseokjung163@gmail.com>
2021-02-03 18:00:02 +0900

    AZAC
    AZ a,c 적용을 위해 수정

--- samyang-erp/aws_subnets.tf
+++ samyang-erp/aws_subnets.tf
@@ -1,12 +1,29 @@
 data "aws_availability_zones" "available" {
   state = "available"
+  filter {
+    name   = "zone-name"
+    values = ["ap-northeast-2a", "ap-northeast-2c"]
+  }
 }
 
-resource "aws_subnet" "sap-hana-sb" {
+resource "aws_subnet" "s4hana-subnet" {
   count = 2
 
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block = cidrsubnet(aws_vpc.samyang_vpc.cidr_block, 8, count.index)
+  cidr_block = cidrsubnet(aws_vpc.samyang_vpc.cidr_block, 12, count.index)
+  vpc_id = aws_vpc.samyang_vpc.id
+
+  tags = {
+    name = "${local.workload}-${op}-subnet"
+    op = "production"
+  }
+}
+
+resource "aws_subnet" "sap-sb" {
+  count = 2
+
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block = cidrsubnet(aws_vpc.samyang_vpc.cidr_block, 12, count.index)
   vpc_id = aws_vpc.samyang_vpc.id
 
   tags = {
--- samyang-erp/local.tf
+++ samyang-erp/local.tf
@@ -1,6 +1,5 @@
 locals {
   service_name = "samyang"
-  owner        = "Community Team"
   workload = "SAP-ERP"
   cidr = "10.200.0.0/16"
 }
