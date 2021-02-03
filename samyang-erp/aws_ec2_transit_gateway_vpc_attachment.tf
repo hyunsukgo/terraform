eae843b0055901825efd8723d26f4119ce395670
Administrator <hyunseokjung163@gmail.com>
2021-02-03 15:12:52 +0900

    Delete aws_ec2_transit_gateway_vpc_attachment.tf
    Attachment 삭제

--- samyang-erp/aws_ec2_transit_gateway_vpc_attachment.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ec2_transit_gateway_vpc_attachment" "samyang_tgw_attachment" {
-  subnet_ids         = [aws_subnet.sap-hana-sb.id]
-  transit_gateway_id = aws_ec2_transit_gateway.samyang_tgw.id
-  vpc_id             = aws_vpc.samyang_vpc.id
-}
