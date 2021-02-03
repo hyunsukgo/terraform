efb28420f37a8a41d2984df71a3d4fb00953b960
Administrator <hyunseokjung163@gmail.com>
2021-02-03 14:44:54 +0900

    VPC & Subnet

--- samyang-erp/aws_subnets.tf
+++ samyang-erp/aws_subnets.tf
@@ -1,11 +1,15 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_subnet" "sap-hana-group" {
   count = 2
 
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
-  vpc_id            = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
-    name = ${local.workload}-subnet
+    name = "${local.workload}-subnet"
   }
 }
--- samyang-erp/aws_vpc.tf
+++ samyang-erp/aws_vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "samyang_vpc" {
-    cidr_block       = "${substr("${local.cidr})"
+    cidr_block       = "${substr("${local.cidr}")"
     instance_tenancy = "default"
     enable_dns_hostnames = "true"
     tags = {
