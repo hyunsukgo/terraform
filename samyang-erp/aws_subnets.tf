40992a3755c2a3bfcaffc7361eff8beb672187a2
Administrator <hyunseokjung163@gmail.com>
2021-02-03 15:02:44 +0900

    Update aws_subnets.tf
    서브넷 오류 수정

--- samyang-erp/aws_subnets.tf
+++ samyang-erp/aws_subnets.tf
@@ -6,8 +6,8 @@ resource "aws_subnet" "sap-hana-group" {
   count = 2
 
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
-  vpc_id = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(aws_vpc.samyang_vpc.cidr_block, 8, count.index)
+  vpc_id = aws_vpc.samyang_vpc.id
 
   tags = {
     name = "${local.workload}-subnet"
