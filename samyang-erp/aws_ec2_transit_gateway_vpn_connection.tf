c26c45ea78a7f2e99a07f4fff94d05d9139b4c4a
Administrator <hyunseokjung163@gmail.com>
2021-02-04 17:09:05 +0900

    오류난 부분 수정
    오류난 부분 수정

--- samyang-erp/aws_ec2_transit_gateway_vpn_connection.tf
+++ samyang-erp/aws_ec2_transit_gateway_vpn_connection.tf
@@ -1,7 +1,4 @@
-resource "aws_ec2_transit_gateway_vpn_attachment" "vpn" {
-  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-   vpn_connection_id  = aws_vpn_connection.vpn.id
-}
+
 resource "aws_vpn_connection" "vpn" {
   customer_gateway_id = aws_customer_gateway.cgw.id
   transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
