d0feb06e6df195ce59aadbdbda7df6ba944c581f
Administrator <hyunseokjung163@gmail.com>
2021-02-04 14:35:52 +0900

    Update aws_vpc_peering.tf
    옵션 제거

--- samyang-erp/aws_vpc_peering.tf
+++ samyang-erp/aws_vpc_peering.tf
@@ -2,12 +2,4 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_owner_id = local.peerid
   peer_vpc_id   = local.peervpcid
   vpc_id        = aws_vpc.vpc.id
-  auto_accept   = true
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
 }
