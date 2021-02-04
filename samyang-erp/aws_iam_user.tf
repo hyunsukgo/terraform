395531d4570e287e23743c8a66f217f0b6a032e3
Administrator <hyunseokjung163@gmail.com>
2021-02-04 12:51:26 +0900

    Update aws_iam_user.tf

--- samyang-erp/aws_iam_user.tf
+++ samyang-erp/aws_iam_user.tf
@@ -5,22 +5,3 @@ resource "aws_iam_user" "architect" {           #담당 ㅁㄱㅊ
     company = "sk C&C"
   }
 }
-
-
-resource "aws_iam_user_policy" "cloud_architect" {
-  name = "Admin"
-  user = aws_iam_user.architect.name
-
-  policy = <<EOF
-  {
-      "Version": "2012-10-17",
-      "Statement": [
-          {
-              "Effect": "Allow",
-              "Action": "*",
-              "Resource": "*"
-          }
-      ]
-  }
-EOF
-}
