d222c6c51ceafd08b40f7181cc2c543c84ba88b7
Administrator <hyunseokjung163@gmail.com>
2021-02-04 11:09:23 +0900

    Update aws_iam_user.tf
    유저 수 설정

--- samyang-erp/aws_iam_user.tf
+++ samyang-erp/aws_iam_user.tf
@@ -1,26 +1,27 @@
-resource "aws_iam_user" "sk_architect" {
-  name = "loadbalancer"
-
+resource "aws_iam_user" "architect" {
+  count = 2               #담당 ㅁㄱㅊ
+  name = "skcc-sa${[count.index]}"
+  
   tags = {
-    tag-key = "tag-value"
+    company = "sk C&C"
   }
 }
 
 
-resource "aws_iam_user_policy" "lb_ro" {
+resource "aws_iam_user_policy" "cloud_architect" {
   name = "Admin"
-  user = aws_iam_user.sk_architect.name
+  user = aws_iam_user.architect.name
 
   policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*"
-        }
-    ]
-}
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": "*",
+              "Resource": "*"
+          }
+      ]
+  }
 EOF
 }
