ee81a747f4f918d7df1486d622646832def95f3b
Administrator <hyunseokjung163@gmail.com>
2021-02-04 15:04:45 +0900

    Update aws_iam_user.tf
    IAM Policy 추가

--- samyang-erp/aws_iam_user.tf
+++ samyang-erp/aws_iam_user.tf
@@ -5,3 +5,27 @@ resource "aws_iam_user" "architect" {
     company = ""
   }
 }
+resource "aws_iam_policy" "admin" {
+  name        = "Architect"
+  path        = "/"
+  description = "Architect policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_policy_attachment" "admin-attach" {
+  name       = "admin-attachment"
+  users      = [aws_iam_user.architect.name]
+  policy_arn = aws_iam_policy.admin.arn
+}
+
