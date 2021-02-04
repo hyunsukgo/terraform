3632bca0b98075c0b39f4f5aa56bbd2edd8be151
Administrator <hyunseokjung163@gmail.com>
2021-02-04 15:15:39 +0900

    오류 수정
    오류 수정

--- samyang-erp/aws_config.tf
+++ samyang-erp/aws_config.tf
@@ -1,4 +1,4 @@
 resource "aws_config_aggregate_authorization" "config" {
   account_id = local.account_id
-  region     = aws.region
+  region     = local.region
 }
--- samyang-erp/local.tf
+++ samyang-erp/local.tf
@@ -5,5 +5,6 @@ locals {
   cgw = "220.72.11.210"
   peerid = "036491913501"
   account_id = "969470234861"
+  region = "ap-northeast-2"
   peervpcid = "vpc-08029084c4e1f5546"
 }
