542ba6da44008f81b4a105d99cf978a86e26ba22
ScottSeungkyunRyu <princeskrr@naver.com>
2021-02-05 17:29:19 +0900

    routingTestSubnetre2

--- samyang-erp/aws_subnets.tf
+++ samyang-erp/aws_subnets.tf
@@ -37,7 +37,7 @@ resource "aws_subnet" "Defalt-subnet" {
 resource "aws_subnet" "Defalt-subnet" {
   vpc_id = aws_vpc.vpc.id
   availability_zone = "ap-northeast-2a"
-  cidr_block        = "10.200.30.112/26"
+  cidr_block        = "10.200.30.64/26"
 
   tags  = {
     Name = "SAPDEV_A"
