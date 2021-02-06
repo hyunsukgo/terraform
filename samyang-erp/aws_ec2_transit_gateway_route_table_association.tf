04d4f8837e9a8f2f83bd607ff45f13a1867a0727
Administrator <hyunseokjung163@gmail.com>
2021-02-06 10:11:27 +0900

    Update aws_ec2_transit_gateway_route_table_association.tf

--- samyang-erp/aws_ec2_transit_gateway_route_table_association.tf
+++ samyang-erp/aws_ec2_transit_gateway_route_table_association.tf
@@ -1,4 +1,4 @@
 resource "aws_ec2_transit_gateway_route_table_association" "tgw_rta" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpn_attachment.tgw_attachment.id
+  transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_vpn_attachment.tgw_attachment.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
 }
