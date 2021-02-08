8774bce0ba16983a5e2b7b8dd058e10740ec7736
Administrator <hyunseokjung163@gmail.com>
2021-02-08 16:11:17 +0900

    Security Group 추가
    설정 추가

---
+++ samyang-erp/aws_security_group.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH from On-Prem"
+    from_port   = 0
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = "130.1.0.0/16"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
