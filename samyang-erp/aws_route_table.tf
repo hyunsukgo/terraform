9ea08796b442ee72a8e14e80dc470d64d4b213d2
Administrator <hyunseokjung163@gmail.com>
2021-02-08 10:35:06 +0900

    Update aws_route_table.tf
    Peering 통신 라우팅 추가

--- samyang-erp/aws_route_table.tf
+++ samyang-erp/aws_route_table.tf
@@ -5,6 +5,10 @@ resource "aws_route_table" "r" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
+  route {
+    cidr_block = "10.100.0.0/0"
+    gateway_id = aws_vpc_peering_connection.peering.id
+  }
 
   tags = {
     Name = "public-rt"
