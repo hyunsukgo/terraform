bcff0d03802eb58fb062b94205f73823ec2281d3
Administrator <hyunseokjung163@gmail.com>
2021-02-08 16:20:08 +0900

    Update aws_security_group.tf

--- samyang-erp/aws_security_group.tf
+++ samyang-erp/aws_security_group.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 0
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "130.1.0.0/16"
+    cidr_blocks = ["130.1.0.0/16"]
   }
 
   egress {
