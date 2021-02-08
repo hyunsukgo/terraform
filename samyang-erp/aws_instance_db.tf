b46217cb26c81a50a7f4f593bb0aa7e5a96ce04d
Administrator <hyunseokjung163@gmail.com>
2021-02-09 01:07:11 +0900

    Update aws_instance_db.tf

--- samyang-erp/aws_instance_db.tf
+++ samyang-erp/aws_instance_db.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "sapdb" {
   ami           = "ami-097fc5cd098dd20d5"
   instance_type = "r5.8xlarge"
   private_ip = "10.200.30.11"
-  security_groups = [aws_security_group.allow_ssh_from_vpn.id]
+  #security_groups = [aws_security_group.allow_ssh_from_vpn.id]
   root_block_device {
     volume_size = 30
     volume_type = "gp3"  
