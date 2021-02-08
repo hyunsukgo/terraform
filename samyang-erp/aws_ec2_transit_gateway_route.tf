b8f76f66e164ef663f2c38f1cd3b160ceff2bec5
Administrator <hyunseokjung163@gmail.com>
2021-02-08 13:14:14 +0900

    Update aws_ec2_transit_gateway_route.tf

--- samyang-erp/aws_ec2_transit_gateway_route.tf
+++ samyang-erp/aws_ec2_transit_gateway_route.tf
@@ -1,5 +1,4 @@
 resource "aws_ec2_transit_gateway_route" "vpn_route" {
   destination_cidr_block         = "130.1.0.0/16"
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpn_attachment.tgw_attachment.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
 }
