2ffc1af49afdbd0eebed7a825d2c7ea051abb8e5
Administrator <hyunseokjung163@gmail.com>
2021-02-08 15:58:17 +0900

    Update aws_ec2_transit_gateway.tf

--- samyang-erp/aws_ec2_transit_gateway.tf
+++ samyang-erp/aws_ec2_transit_gateway.tf
@@ -1,5 +1,8 @@
 resource "aws_ec2_transit_gateway" "tgw" {
   description = "${local.service_name}-tgw"
+  default_route_table_association = aws_ec2_transit_gateway_route_table.tgw_rt.id
+  default_route_table_propagation = aws_ec2_transit_gateway_route_table.tgw_rt.id
+
   tags = {
       Name = "${local.service_name}-tgw"
   }
