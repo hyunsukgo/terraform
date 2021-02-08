87da6758b889f9e83e0bf901cf97ff6957c588cf
Administrator <hyunseokjung163@gmail.com>
2021-02-08 15:50:19 +0900

    Update aws_route_table.tf

--- samyang-erp/aws_route_table.tf
+++ samyang-erp/aws_route_table.tf
@@ -9,14 +9,13 @@ resource "aws_route_table" "r" {
     cidr_block = "10.100.0.0/16"
     gateway_id = aws_vpc_peering_connection.peering.id
   }
-  /*
   route {
     cidr_block = "130.1.0.0/16"
     gateway_id = aws_ec2_transit_gateway.tgw.id
   }
-  */
+  
 
   tags = {
-    Name = "public-rt"
+    Name = "${local.service_name}-public-rt"
   }
 }
