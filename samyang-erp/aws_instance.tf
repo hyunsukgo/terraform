be0257147353d4bf118d9fa40475d981e48807db
Administrator <hyunseokjung163@gmail.com>
2021-02-08 17:51:52 +0900

    Update aws_instance.tf

--- samyang-erp/aws_instance.tf
+++ samyang-erp/aws_instance.tf
@@ -1,10 +1,21 @@
 
 resource "aws_instance" "sapap" {
-  ami           = ami-097fc5cd098dd20d5
+  ami           = "ami-097fc5cd098dd20d5"
   instance_type = "m5.2xlarge"
   private_ip = "10.200.30.12"
   tags = {
     Name = "sapap1"
+    Enviroment = "Dev"
+  }
+  #user_data = "${file("sapinst.sh")}"
+  subnet_id = aws_subnet.SAPDEV_A.id
+}
+resource "aws_instance" "sapdb" {
+  ami           = "ami-097fc5cd098dd20d5"
+  instance_type = "m5.8xlarge"
+  private_ip = "10.200.30.11"
+  tags = {
+    Name = "sapdb1"
     Enviroment = "Dev"
   }
   #user_data = "${file("sapinst.sh")}"
