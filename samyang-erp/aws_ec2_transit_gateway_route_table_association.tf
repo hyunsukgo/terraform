0e8e2f1b4dca0b0a78bd424aaee30a9faba008a5
Administrator <hyunseokjung163@gmail.com>
2021-02-08 14:28:20 +0900

    Update aws_ec2_transit_gateway_route_table_association.tf
    오류 수정

--- samyang-erp/aws_ec2_transit_gateway_route_table_association.tf
+++ samyang-erp/aws_ec2_transit_gateway_route_table_association.tf
@@ -1,5 +1,5 @@
 resource "aws_ec2_transit_gateway_route_table_association" "tgw_rta_vpn" {
-  transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_vpn_attachment.tgw_attachment.id
+  transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_vpn_attachment.tgw_attachment_vpn.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
 }
 resource "aws_ec2_transit_gateway_route_table_association" "tgw_rta_vpc" {
