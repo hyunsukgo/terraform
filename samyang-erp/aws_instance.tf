e986d14f3caaae966b5b7e6ca538ccf4d6c5c0f5
Administrator <hyunseokjung163@gmail.com>
2021-02-08 20:18:24 +0900

    Update aws_instance.tf
    EBS 볼륨 추가

--- samyang-erp/aws_instance.tf
+++ samyang-erp/aws_instance.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "aws_instance" "sapap" {
   ami           = "ami-097fc5cd098dd20d5"
   instance_type = "m5.2xlarge"
@@ -11,7 +11,48 @@ resource "aws_instance" "sapap" {
   associate_public_ip_address = false
   #user_data = ${file("sapinst.sh")}
   subnet_id = aws_subnet.SAPDEV_A.id
-}/*
+}
+resource "aws_volume_attachment" "sapmnt_att" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.sapmnt.id
+  instance_id = aws_instance.sapap.id
+}
+resource "aws_volume_attachment" "usrsap_att" {
+  device_name = "/dev/sdc"
+  volume_id   = aws_ebs_volume.usrsap.id
+  instance_id = aws_instance.sapap.id
+}
+resource "aws_volume_attachment" "usrsaptrans_att" {
+  device_name = "/dev/sdd"
+  volume_id   = aws_ebs_volume.usrsaptrans.id
+  instance_id = aws_instance.sapap.id
+}
+resource "aws_volume_attachment" "sapcd_ap_att" {
+  device_name = "/dev/sde"
+  volume_id   = aws_ebs_volume.sapcd-ap.id
+  instance_id = aws_instance.sapap.id
+}
+resource "aws_ebs_volume" "sapmnt" {
+  availability_zone = "${local.region}-a"
+  size              = 10
+  iops              = gp3
+}
+resource "aws_ebs_volume" "usrsap" {
+  availability_zone = "${local.region}-a"
+  size              = 20
+  iops              = gp3
+}
+resource "aws_ebs_volume" "usrsaptrans" {
+  availability_zone = "${local.region}-a"
+  size              = 100
+  iops              = gp3
+}
+resource "aws_ebs_volume" "sapcd-ap" {
+  availability_zone = "${local.region}-a"
+  size              = 2048
+  iops              = gp3
+}
+/*
 resource "aws_instance" "sapdb" {
   ami           = "ami-097fc5cd098dd20d5"
   instance_type = "m5.8xlarge"
