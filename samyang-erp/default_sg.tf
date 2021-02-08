1ff9acb70f63e446dc1e14b7ed8d41e035b78c11
ScottSeungkyunRyu <princeskrr@naver.com>
2021-02-08 16:52:59 +0900

    launchConfiguration

---
+++ samyang-erp/default_sg.tf
@@ -0,0 +1,21 @@
+resource "aws_default_security_group" "sy_d_sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dev-default_user03"
+  }
+}
