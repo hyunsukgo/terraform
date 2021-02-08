5ad79ddf2bd10e7d3d61691723a658dc1b548c9f
Administrator <hyunseokjung163@gmail.com>
2021-02-08 17:17:39 +0900

    Update aws_launch_configuration.tf

--- samyang-erp/aws_launch_configuration.tf
+++ samyang-erp/aws_launch_configuration.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "sedap" {
   instance_type = "m5.2xlarge"
   key_name = "test"
   #vpc_id = aws_vpc.vpc.id
-  security_groups = "${aws_default_security_group.sy_d_sg.id}"
+  #security_groups = "${aws_default_security_group.sy_d_sg.id}"
   #subnet_id = "${aws_subnet.SAPAP1_A.id}"
   /*위에서 생성한 PEM키를 어떻게 변환해야 되는지 확인 중*/
   #private_ips = ["10.200.30.12"] # aws_instance 리소스에 적용 가능
