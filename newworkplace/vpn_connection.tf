404ba3f311feeee9ca175aeff50b084879007563
Administrator <hyunseokjung163@gmail.com>
2021-02-15 14:34:57 +0900

    Delete vpn_connection.tf

--- newworkplace/vpn_connection.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "redshift-subnet-a" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "${substr("${local.cidr}", 0, 4)}.1.0/25"
-  availability_zone = "ap-northeast-2a"
-  tags = {
-    Name = "${local.service_name}-redshift-subnet-a"
-  }
-}
