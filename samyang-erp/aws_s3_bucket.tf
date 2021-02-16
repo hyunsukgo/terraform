9c9a7f29361567ad3d3ee460b2b54664cb235576
Administrator <hyunseokjung163@gmail.com>
2021-02-17 01:13:52 +0900

    Update aws_s3_bucket.tf
    Cloud Trail Bucket 관련 오류 수정

--- samyang-erp/aws_s3_bucket.tf
+++ samyang-erp/aws_s3_bucket.tf
@@ -1,6 +1,38 @@
 resource "aws_s3_bucket" "cloudtrailucket" {
   bucket = "${local.service_name}-cloudtrail-bucket"
-  
+   force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::${local.service_name}-cloudtrail-bucket"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${local.service_name}-cloudtrail-bucket/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
   tags = {
     Name        = "${local.service_name}-bucket"
     Environment = "Prd"
