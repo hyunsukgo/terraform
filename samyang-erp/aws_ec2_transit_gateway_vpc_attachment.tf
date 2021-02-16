dfaaf40b6879a34c7005a1939bdde72cccd20666
Administrator <hyunseokjung163@gmail.com>
2021-02-16 15:16:01 +0900

    Add KMS

--- samyang-erp/aws_ec2_transit_gateway_vpc_attachment.tf
+++ samyang-erp/aws_ec2_transit_gateway_vpc_attachment.tf
@@ -1,4 +1,4 @@
-resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_vpc" {
   subnet_ids         = [aws_subnet.COMMON1_A.id]
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   vpc_id             = aws_vpc.vpc.id
