b8e144384051dbc06b1150186e77038f7872f8d4
Administrator <hyunseokjung163@gmail.com>
2021-02-17 01:00:05 +0900

    Update aws_s3_bucket.tf

--- samyang-erp/aws_s3_bucket.tf
+++ samyang-erp/aws_s3_bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "cloudtrailucket" {
   bucket = "${local.service_name}-cloudtrail-bucket"
-  acl    = "private"
+  acl    = "log-delivery-write"
 
   tags = {
     Name        = "${local.service_name}-bucket"
