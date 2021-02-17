3786f5e492568f1539f71c47f1288f1651b34a33
Administrator <hyunseokjung163@gmail.com>
2021-02-17 10:43:16 +0900

    Create aws_private_route_table_association.tf
    Private Routing Association 추가

---
+++ samyang-erp/aws_private_route_table_association.tf
@@ -0,0 +1,13 @@
+data "aws_subnet_ids" "private_subnets" {
+  vpc_id = aws_vpc.vpc.id
+  filter =
+  {
+    RT = "private"
+  }
+}
+
+resource "aws_route_table_association" "private_rta" {
+  count          = length(data.aws_subnet_ids.private_subnets.ids)
+  subnet_id      = data.aws_subnet_ids.private_subnets.ids[count.index]
+  route_table_id = aws_route_table.private_route.id
+}
