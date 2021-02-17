44f591c7996d193b8b641073850e922ff6b18216
Administrator <hyunseokjung163@gmail.com>
2021-02-17 11:17:37 +0900

    Update aws_private_route_table_association.tf

--- samyang-erp/aws_private_route_table_association.tf
+++ samyang-erp/aws_private_route_table_association.tf
@@ -6,6 +6,6 @@ data "aws_subnet" "private_subnets" {
 }
 
 resource "aws_route_table_association" "private_rta" {
-  subnet_id      = data.aws_subnet.private_subnets.id[*]
+  subnet_id      = [data.aws_subnet.private_subnets.id[*]]
   route_table_id = aws_route_table.private_route.id
 }
