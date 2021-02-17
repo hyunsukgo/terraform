e95f738ca258fe5d04efc35f403edc96eb210e9d
Administrator <hyunseokjung163@gmail.com>
2021-02-17 15:44:24 +0900

    Update aws_iam_group.tf
    으어?

--- samyang-erp/aws_iam_group.tf
+++ samyang-erp/aws_iam_group.tf
@@ -18,23 +18,3 @@ resource "aws_iam_group" "SCIENTIST" {
   name = "SCIENTIST"
   path = "/users/"
 }
-
-resource "aws_iam_group_policy" "my_developer_policy" {
-  name  = "my_developer_policy"
-  group = aws_iam_group.my_developers.name
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
