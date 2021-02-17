4fb37d77ce8395a08c1be553f539de24171b5fcb
Administrator <hyunseokjung163@gmail.com>
2021-02-17 11:25:59 +0900

    Update aws_private_route_table_association.tf

--- samyang-erp/aws_private_route_table_association.tf
+++ samyang-erp/aws_private_route_table_association.tf
@@ -1,11 +1,13 @@
-data "aws_subnet" "private_subnets" {
-  filter {
-    name = "tag:RT"
-    values = ["private"]
+data "aws_subnet_ids" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Tier = "private"
   }
 }
 
 resource "aws_route_table_association" "private_rta" {
-  subnet_id      = [data.aws_subnet.private_subnets.id[*]]
+  for_each      = data.aws_subnet_ids.private.ids
+  subnet_id      = each.value
   route_table_id = aws_route_table.private_route.id
 }
