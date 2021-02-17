492ed28874d4049082837b00e54b7f10235167e7
Administrator <hyunseokjung163@gmail.com>
2021-02-17 10:56:01 +0900

    Update aws_private_route_table_association.tf
    추가

--- samyang-erp/aws_private_route_table_association.tf
+++ samyang-erp/aws_private_route_table_association.tf
@@ -1,5 +1,4 @@
-data "aws_subnet_ids" "private_subnets" {
-  vpc_id = aws_vpc.vpc.id
+data "aws_subnet_id" "private_subnets" {
   filter {
     name = "tag:RT"
     values = ["private"]
@@ -7,7 +6,7 @@ data "aws_subnet_ids" "private_subnets" {
 }
 
 resource "aws_route_table_association" "private_rta" {
-  count          = length(data.aws_subnet_ids.private_subnets.ids)
-  subnet_id      = data.aws_subnet_ids.private_subnets.ids[count.index]
+  count          = length(data.aws_subnet_id.private_subnets.ids)
+  subnet_id      = data.aws_subnet_ids.private_subnets.id[count.index]
   route_table_id = aws_route_table.private_route.id
 }
