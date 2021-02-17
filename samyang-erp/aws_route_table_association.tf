4ee9d8118fecb8df413cb1116bef9937bf945697
Administrator <hyunseokjung163@gmail.com>
2021-02-17 22:44:57 +0900

    라우팅테이블 수정
    라우팅테이블 수정

--- samyang-erp/aws_route_table_association.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.SAPDEV_A.id
-  route_table_id = aws_route_table.r.id
-}
--- samyang-erp/aws_subnets.tf
+++ samyang-erp/aws_subnets.tf
@@ -3,112 +3,121 @@ data "aws_availability_zones" "available" {
   state = "available"
   filter {
     name   = "zone-name"
-    values = ["ap-northeast-2a", "ap-northeast-2c"]
+    values = ["${local.region}a", "${local.region}c"]
   }
 }
 
 resource "aws_subnet" "SAPDB1_A" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "${local.region}a"
   cidr_block        = "10.200.10.0/26"
 
   tags  = {
     Name = "SAPDB1_A"
     Enviroment = "PRD"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "SAPAP1_A" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "${local.region}a"
   cidr_block        = "10.200.20.0/26"
 
   tags  = {
     Name = "SAPAP1_A"
     Enviroment = "PRD"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "SAPDB2_C" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2c"
+  availability_zone = "${local.region}c"
   cidr_block        = "10.200.11.0/26"
 
   tags  = {
     Name = "SAPDB2_C"
     Enviroment = "PRD"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "SAPAP2_C" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2c"
+  availability_zone = "${local.region}c"
   cidr_block        = "10.200.21.0/26"
 
   tags  = {
     Name = "SAPAP2_C"
     Enviroment = "PRD"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "SAPDEV_A" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "${local.region}a"
   cidr_block        = "10.200.30.0/26"
 
   tags  = {
     Name = "SAPDEV_A"
     Enviroment = "DEV"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "LEGADB1_A" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "${local.region}a"
   cidr_block        = "10.200.40.0/25"
 
   tags  = {
     Name = "LEGADB1_A"
     Enviroment = "PRD"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "LEGAAP1_A" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "${local.region}a"
   cidr_block        = "10.200.50.0/25"
 
   tags  = {
     Name = "LEGAAP1_A"
     Enviroment = "PRD"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "LEGADMZ_A" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "${local.region}a"
   cidr_block        = "10.200.50.240/28"
 
   tags  = {
     Name = "LEGADMZ_A"
     Enviroment = "PRD"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "LEGADEV_A" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "${local.region}a"
   cidr_block        = "10.200.60.0/26"
 
   tags  = {
     Name = "LEGADEV_A"
     Enviroment = "DEV"
+    RT   = "private"
   }
 }
 
 resource "aws_subnet" "COMMON1_A" {
   vpc_id = aws_vpc.vpc.id
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "${local.region}a"
   cidr_block        = "10.200.0.0/26"
 
   tags  = {
@@ -123,7 +132,7 @@ resource "aws_subnet" "COMMON2_C" {
   cidr_block        = "10.200.0.64/26"
 
   tags  = {
-    Name = "COMMON1_A"
+    Name = "COMMON2_C"
     RT   = "public"
     Enviroment = "PRD_NW"
   }
