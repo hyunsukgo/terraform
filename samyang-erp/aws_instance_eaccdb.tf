3d13b4cb1b97666c963445dd2b73975a1b725495
Administrator <hyunseokjung163@gmail.com>
2021-02-19 01:33:03 +0900

    Update aws_instance_eaccdb.tf

--- samyang-erp/aws_instance_eaccdb.tf
+++ samyang-erp/aws_instance_eaccdb.tf
@@ -9,8 +9,8 @@ resource "aws_instance" "eaccdb"   {
     volume_type = "gp2"
     volume_size = 50
   }
-
-  security_groups = [aws_security_group.allow_from_trust_to_eaccdb.id]
+  disable_api_termination = "true"
+  #security_groups = [aws_security_group.allow_from_trust_to_eaccdb.id]
   subnet_id = aws_subnet.LEGADMZ_A.id
 
   tags = {
