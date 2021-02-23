ba7527b4e0465ef220f2c8a771601b4cfd2059d0
Administrator <hyunseokjung163@gmail.com>
2021-02-23 17:43:24 +0900

    aws_backup_vault_plan 구성
    aws_backup_vault_plan 구성

--- samyang-erp/aws_backup.tf
+++ samyang-erp/aws_backup.tf
@@ -2,3 +2,20 @@ resource "aws_backup_vault" "backup" {
   name        = "${local.service_name}_backup_vault"
   kms_key_arn = aws_kms_key.backup.arn
 }
+
+resource "aws_backup_plan" "backup_plan" {
+  name = "${local.service_name}_backup_plan"
+
+  rule {
+    rule_name         = "${local.service_name}_example_backup_rule"
+    target_vault_name = aws_backup_vault.backup.name
+    schedule          = "cron(0 12 * * ? *)"
+  }
+
+  advanced_backup_setting {
+    backup_options = {
+      WindowsVSS = "enabled"
+    }
+    resource_type = "EC2"
+  }
+}
