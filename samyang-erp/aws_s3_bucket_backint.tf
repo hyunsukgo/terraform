48127200da28d910f5bb4f1a95c2252e67f2db40
Administrator <hyunseokjung163@gmail.com>
2021-02-23 11:16:36 +0900

    Update aws_s3_bucket_backint.tf
    Bucket Policy 수정

--- samyang-erp/aws_s3_bucket_backint.tf
+++ samyang-erp/aws_s3_bucket_backint.tf
@@ -2,38 +2,6 @@ resource "aws_s3_bucket" "backintbucket" {
   bucket        = "${local.service_name}-backint-bucket"
   force_destroy = true
 
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor1",
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetBucketPolicyStatus",
-                "s3:GetBucketLocation",
-                "s3:ListBucket",
-                "s3:GetBucketAcl",
-                "s3:GetBucketPolicy"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${local.service_name}-backint-bucket*"
-            ]
-        },
-        {
-              "Sid": "VisualEditor0",
-              "Effect": "Allow",
-              "Action": [
-                  "s3:PutObjectTagging",
-                  "s3:PutObject",
-                  "s3:GetObject",
-                  "s3:DeleteObject"
-              ],
-              "Resource": "arn:aws:s3:::${local.service_name}-backint-bucket*"
-        }
-    ]
-}
-POLICY
   tags = {
     Name        = "${local.service_name}-bucket"
     Environment = "Prd"
