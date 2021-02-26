d603921736dbb271c2c64bc76c2261a384260f05
Administrator <hyunseokjung163@gmail.com>
2021-02-26 09:58:23 +0900

    Backup Selection 추가

--- samyang-erp/aws_backup.tf
+++ samyang-erp/aws_backup.tf
@@ -1,10 +1,10 @@
 resource "aws_backup_vault" "backup" {
-  name        = "${local.service_name}_backup_vault"
+  name        = "${local.service_name}-backup-vault"
   kms_key_arn = aws_kms_key.backup.arn
 }
 
-resource "aws_backup_plan" "backup_plan" {
-  name = "${local.service_name}_backup_plan"
+resource "aws_backup_plan" "backupplan" {
+  name = "${local.service_name}-backup-plan"
 
   rule {
     rule_name         = "${local.service_name}_example_backup_rule"
@@ -17,5 +17,34 @@ resource "aws_backup_plan" "backup_plan" {
       WindowsVSS = "enabled"
     }
     resource_type = "EC2"
+  }
+}
+
+resource "aws_iam_role" "backuprole" {
+  name               = "${local.service_name}-backup-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["sts:AssumeRole"],
+      "Effect": "allow",
+      "Principal": {
+        "Service": ["backup.amazonaws.com"]
+      }
+    }
+  ]
+}
+POLICY
+}
+resource "aws_backup_selection" "backselection" {
+  iam_role_arn = aws_iam_role.backuprole.arn
+  name         = "${local.service_name}-selection"
+  plan_id      = aws_backup_plan.backupplan.id
+
+  selection_tag {
+    type  = "STRINGEQUALS"
+    key   = "foo"
+    value = "bar"
   }
 }
