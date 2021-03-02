435dcaa6f878add25ef4a0f99c4664b11e18c5bb
Administrator <hyunseokjung163@gmail.com>
2021-03-02 13:53:01 +0900

    instnace -> instances

--- samyang-erp/aws_elb.tf
+++ samyang-erp/aws_elb.tf
@@ -9,7 +9,7 @@ resource "aws_lb_target_group" "reporttg" {
 }
 
 
-data "aws_instance" "report" {
+data "aws_instances" "report" {
   filter {
     name   = "tag:Name"
     values = ["sy-rptab01","sy-rptab02"]
@@ -17,7 +17,7 @@ data "aws_instance" "report" {
 }
 
 resource "aws_lb_target_group_attachment" "reporttga" {
-  for_each         = data.aws_instance.report.id
+  for_each         = data.aws_instance.report.ids
   target_group_arn = aws_lb_target_group.reporttg.arn
   target_id        = each.value
   port             = 80
