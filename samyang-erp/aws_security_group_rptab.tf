0c12351215ecd69919e28a453c8cec0d38e2a318
Administrator <hyunseokjung163@gmail.com>
2021-03-02 16:19:47 +0900

    80이 아니었네?

--- samyang-erp/aws_security_group_rptab.tf
+++ samyang-erp/aws_security_group_rptab.tf
@@ -5,9 +5,17 @@ resource "aws_security_group" "allow_from_trust_to_rptab" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description = "Access from ALB(HTTP)"
+    description = "Access for RDP"
     from_port   = 3389
     to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
+  }
+
+  ingress {
+    description = "Access for RDP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     #cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
     security_groups = [aws_security_group.allow_from_trust_to_report_alb.id]
