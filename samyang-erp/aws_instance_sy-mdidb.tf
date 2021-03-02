d504a311a7f2570407c5d6ebfd86b440af934411
Administrator <hyunseokjung163@gmail.com>
2021-03-02 09:37:17 +0900

    MDIDB 인스턴스 추가

---
+++ samyang-erp/aws_instance_sy-mdidb.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "mdidb" {
+  ami           = "ami-0fc1ba861e48fc890" # ap-northeast-2
+  instance_type = "c5.2xlarge"
+
+  private_ip = "10.200.40.61"
+  key_name   = "samyangerp"
+  iam_instance_profile =  "ssm"
+  user_data = "${file("wininst.sh")}"
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 50
+  }
+  disable_api_termination = "true"
+  security_groups = [aws_security_group.allow_from_trust_to_mdidb.id]
+  subnet_id = aws_subnet.LEGADB1_A.id
+
+  tags = {
+    Name        = "sy-mdidb"
+    Description = "중계서버 인스턴스"
+    Environment = "Prd"
+    cz-product  = "Non-SAP"
+    Schedule    = "samyang-office-hours"
+  }
+}
+
+resource "aws_volume_attachment" "rptab02_att" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.mdidb_add.id
+  instance_id = aws_instance.mdidb.id
+}
+
+resource "aws_ebs_volume" "mdidb_add" {
+  availability_zone = "${local.region}a"
+  size              = 100
+  type              = "gp2"
+  tags = {
+    Snapshot = "true"
+  }
+}
---
+++ samyang-erp/aws_security_group_mdidb.tf
@@ -0,0 +1,24 @@
+
+resource "aws_security_group" "allow_from_trust_to_mdidb" {
+  name        = "allow_traffic_mdidb"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "RDP from On-Prem"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_from_trust_to_mdidb"
+  }
+} 
