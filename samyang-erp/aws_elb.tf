b24d708c21b6c753e06067e911db20bb5b2df293
Administrator <hyunseokjung163@gmail.com>
2021-03-02 13:49:27 +0900

    수정해봄#2

--- samyang-erp/aws_elb.tf
+++ samyang-erp/aws_elb.tf
@@ -17,7 +17,7 @@ data "aws_instance" "report" {
 }
 
 resource "aws_lb_target_group_attachment" "reporttga" {
-  for_each         = data.aws_instance.report.ids
+  for_each         = data.aws_instance.report.id
   target_group_arn = aws_lb_target_group.reporttg.arn
   target_id        = each.value
   port             = 80
