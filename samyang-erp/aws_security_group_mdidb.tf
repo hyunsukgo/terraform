7880c989fd3dce68e3490520d619fafb7b0928c1
Administrator <hyunseokjung163@gmail.com>
2021-03-05 11:05:12 +0900

    콘솔에서 추가된 SG 적용

--- samyang-erp/aws_security_group_mdidb.tf
+++ samyang-erp/aws_security_group_mdidb.tf
@@ -5,6 +5,14 @@ resource "aws_security_group" "allow_from_trust_to_mdidb" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
+    description = "MSSQL Access from On-Prem"
+    from_port   = 1433
+    to_port     = 1433
+    protocol    = "tcp"
+    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
+  }
+  
+  ingress {
     description = "RDP from On-Prem"
     from_port   = 3389
     to_port     = 3389
