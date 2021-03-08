0e92cfd41097ac99cd7e5c068f06249c0f742a50
Administrator <hyunseokjung163@gmail.com>
2021-03-08 09:53:46 +0900

    HTTPS 추가

--- samyang-erp/aws_security_group_saprouter.tf
+++ samyang-erp/aws_security_group_saprouter.tf
@@ -18,6 +18,13 @@ resource "aws_security_group" "allow_from_trust_to_saprouter" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "SapRouter Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
