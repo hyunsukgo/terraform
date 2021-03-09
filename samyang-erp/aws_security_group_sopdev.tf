dafecd3c84e4defdc48e36313044a8674867a300
Administrator <hyunseokjung163@gmail.com>
2021-03-09 15:07:24 +0900

    S&OP 개발 서버 SG 그룹 Port 추가(5000,8080)

--- samyang-erp/aws_security_group_sopdev.tf
+++ samyang-erp/aws_security_group_sopdev.tf
@@ -12,6 +12,22 @@ resource "aws_security_group" "allow_from_trust_to_sopdev" {
     cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
   }
   ingress {
+    description = "Application"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
+  }
+
+  ingress {
+    description = "Application"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
+  }
+
+  ingress {
     description = "MSSQL Access from On-Prem"
     from_port   = 1433
     to_port     = 1433
