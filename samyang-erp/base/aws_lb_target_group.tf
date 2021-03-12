1fc61ab3157bee541661a0b36bc2da72028f61bc
Administrator <hyunseokjung163@gmail.com>
2021-03-12 13:44:03 +0900

    test

--- samyang-erp/base/aws_lb_target_group.tf
+++ samyang-erp/base/aws_lb_target_group.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "rttabtg" {
-  name     = "${var.service_name}-rttab-lb-tg"
+  name     = "rttab-lb-tg"
   port     = 80
   protocol = "HTTP"
   #vpc_id   = aws_vpc.vpc.id
