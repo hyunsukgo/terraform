ff425b3fe628bcb1757155e777b42aaf150dfcc9
Administrator <hyunseokjung163@gmail.com>
2021-03-12 15:31:12 +0900

    test

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -3,6 +3,5 @@ data "aws_instances" "ec2list" {
 
 output "id" {
   description = "List of IDs of instances"
-  count       = length(data.aws_instances.ec2list.ids)
-  value       = data.aws_instances.ec2list.ids[count.index]
+  value       = data.aws_instances.ec2list.ids
 }
