c6e33a0bb89f0f53ceb94482db1f53573f4c32c0
Administrator <hyunseokjung163@gmail.com>
2021-03-16 15:06:48 +0900

    Backup 버킷 라이프라이클 적용

--- samyang-erp/aws_s3_bucket_backup.tf
+++ samyang-erp/aws_s3_bucket_backup.tf
@@ -6,4 +6,13 @@ resource "aws_s3_bucket" "backupbucket" {
     Name        = "${var.service_name}-backup-bucket"
     Environment = "Prd"
   }
+  lifecycle_rule {
+    id      = "SY-bucket-LR"
+    enabled = true
+
+    expiration {
+      days = 5
+      expired_object_delete_marker = false
+    }
+  }
 }
