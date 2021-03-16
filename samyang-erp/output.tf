b9a52aabeddd4d3da52b877f1c9b9faaca8d0a7d
Administrator <hyunseokjung163@gmail.com>
2021-03-16 14:59:43 +0900

    Output 뺌

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -1,7 +1,8 @@
-data "aws_instances" "ec2list" {
+/*data "aws_instances" "ec2list" {
 }
 
 output "id" {
   description = "List of IDs of instances"
   value       = data.aws_instances.ec2list.ids
 }
+*/
