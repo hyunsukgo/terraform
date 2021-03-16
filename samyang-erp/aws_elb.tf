bedf12472ab70c8a75fde65469b279591857dbcb
Administrator <hyunseokjung163@gmail.com>
2021-03-16 15:40:46 +0900

    뭐지?

--- samyang-erp/aws_elb.tf
+++ samyang-erp/aws_elb.tf
@@ -56,6 +56,7 @@ resource "aws_security_group" "allow_from_trust_to_report_alb" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["10.200.0.0/16"]
+    # 머냐
     prefix_list_ids = [aws_ec2_managed_prefix_list.aempl.id]
   }
   egress {
