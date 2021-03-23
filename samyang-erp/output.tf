be6c82c9e4ce4a8c3bea428803d8bc9ef085cb63
Administrator <hyunseokjung163@gmail.com>
2021-03-23 17:42:32 +0900

    얍얍

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -10,6 +10,6 @@ data "aws_subnet" "subnets" {
 
 output "subnet_cidr_blocks" {
   #value = [for s in data.aws_subnet.subnets : s.cidr_block]
-  value = "${formatlist("%s",(for s in data.aws_subnet.subnets : s.cidr_block)
+  value = "${formatlist("%s",(aws_instance.*.instance_type)
   )}"
 }
