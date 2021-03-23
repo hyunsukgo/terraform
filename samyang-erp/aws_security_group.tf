0e494f159f62a74a05a2c6f9bd89ae2059805f1d
Administrator <hyunseokjung163@gmail.com>
2021-03-24 00:54:41 +0900

    Go

--- samyang-erp/aws_security_group.tf
+++ samyang-erp/aws_security_group.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "allow_from_trust_to_ap" {
     to_port     = 3699
     protocol    = "tcp"
     prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
-  }/*
+  }
   ingress {
     description = "From On-Prem"
     from_port   = 4237
@@ -132,7 +132,7 @@ resource "aws_security_group" "allow_from_trust_to_ap" {
     to_port     = 59999
     protocol    = "tcp"
     prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
-  }*/
+  }
   egress {
     from_port   = 0
     to_port     = 0
