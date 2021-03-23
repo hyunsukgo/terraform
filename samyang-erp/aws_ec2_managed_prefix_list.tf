05f72b2e43b97fd0de67a9218570e5d2f6ad5a7c
Administrator <hyunseokjung163@gmail.com>
2021-03-23 14:36:36 +0900

    주석 제거

--- samyang-erp/aws_ec2_managed_prefix_list.tf
+++ samyang-erp/aws_ec2_managed_prefix_list.tf
@@ -1,19 +1,4 @@
-/*
-resource "aws_ec2_managed_prefix_list" "aempl" {
-  name           = "${var.service_name} On-Prem CIDR-s"
-  address_family = "IPv4"
-  max_entries    = 5
 
-  entry {
-    cidr        = "130.1.0.0/16"
-    description = "VPC CIDR"
-  }
-
-  tags = {
-    Env = "live"
-  }
-}
-*/
 resource "aws_ec2_managed_prefix_list" "trusted" {
   name           = "${var.service_name} Trusted CIDR-s"
   address_family = "IPv4"
