ef003ec1a28840a67e5fcabbeee2c25f2aa2b30d
Administrator <hyunseokjung163@gmail.com>
2021-03-24 00:18:10 +0900

    Test

--- samyang-erp/aws_security_group.tf
+++ samyang-erp/aws_security_group.tf
@@ -163,8 +163,7 @@ resource "aws_security_group" "allow_from_trust_to_db" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
-    ##prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
+    prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
   }
   ingress {
     description = "Host agent access"
