00e2d0c66767ec9990da0a254c1108ab0508c70f
Administrator <hyunseokjung163@gmail.com>
2021-03-24 13:40:25 +0900

    Go Alive

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -27,7 +27,7 @@ data "aws_instances" "ec2" {
 
 data "aws_instance" "ec2" {
   for_each = data.aws_instances.ec2.ids[*]
-  id       = each.value
+  instance_id       = each.value
 }
 output "aws_ec2_instance_types" {
   #value = "${formatlist("%s",(data.aws_instances.ec2.ids[*]))}"
