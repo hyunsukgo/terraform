77ad4c725507af0de5b7c95d252c80f65e386a3e
Administrator <hyunseokjung163@gmail.com>
2021-03-24 18:19:43 +0900

    테스트

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -46,5 +46,5 @@ data "aws_ebs_snapshot" "ebs_volume" {
 
 output "aws_ebs_snapshot_info" {
   #value = formatlist("%s",[for name in data.aws_ebs_snapshot.ebs_volume : name.id])
-  value = formatlist("%s",data.aws_ebs_snapshot.ebs_volume[*].id)
+  value = formatlist("%s",data.aws_ebs_snapshot.ebs_volume[*])
 }
