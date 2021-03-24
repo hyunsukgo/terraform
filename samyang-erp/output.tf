7eb74e9bf5ba0c42ea58b22c87d8477c14feef2b
Administrator <hyunseokjung163@gmail.com>
2021-03-24 11:31:45 +0900

    Filter 추가

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -12,6 +12,9 @@ output "subnet_cidr_blocks" {
   value = [for s in data.aws_subnet.subnets : s.cidr_block]
 }
 data "aws_instances" "ec2list" {
+  instance_tags = {
+    Name = "sy-*"
+  }
   instance_state_names = ["running", "stopped"]
 }
 data "aws_instance" "ec2" {
