c8a467dc76443224705de6cb7a1f3b170577918a
Administrator <hyunseokjung163@gmail.com>
2021-03-24 13:06:10 +0900

    ID

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -18,13 +18,13 @@ data "aws_instances" "ec2list" {
   }
   instance_state_names = ["running", "stopped"]
 }*/
-data "aws_instance" "ec2" {
+data "aws_instances" "ec2" {
   filter {
     name = "tag:Snapshot"
     values = ["Yes"]
   }
 }
 output "aws_ec2_instance_types" {
-  value = [for i in data.aws_instance.ec2 : i.arn]
+  value = [for i in data.aws_instances.ec2 : i.id]
 }
 
