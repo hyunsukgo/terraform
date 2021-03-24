06f993602872a675e89edbfa0199c1ad81ef041a
Administrator <hyunseokjung163@gmail.com>
2021-03-24 17:10:35 +0900

    Most Recent 추가

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -34,7 +34,8 @@ output "aws_ec2_ebs_info" {
 */
 
 data "aws_ebs_snapshot" "ebs_volume" {
-
+  most_recent = true
+  owners      = ["self"]
   filter {
     name   = "tag:Name"
     values = ["sy-*"]
