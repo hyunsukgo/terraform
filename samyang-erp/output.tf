8ddfccdf94ef373b8274f4f5d4500fff50d66c6e
Administrator <hyunseokjung163@gmail.com>
2021-03-24 12:19:22 +0900

    aws_instances

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -16,12 +16,12 @@ data "aws_instances" "ec2list" {
     Name = "sy-eaccdev"
   }
   instance_state_names = ["running", "stopped"]
-}
+}/*
 data "aws_instance" "ec2" {
   for_each = tolist([data.aws_instances.ec2list.ids])
   instance_id       = each.key
-}
+}*/
 output "aws_ec2_instance_types" {
-  value = [for i in data.aws_instance.ec2 : i.arn]
+  value = [for i in data.aws_instances.ec2list : i.arn]
 }
 
