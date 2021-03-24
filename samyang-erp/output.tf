6d7df094e648ee4652bdc1d164773f7a73ddc437
Administrator <hyunseokjung163@gmail.com>
2021-03-24 12:16:16 +0900

    ㄲ

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -15,13 +15,7 @@ data "aws_instances" "ec2list" {
   instance_tags = {
     Name = "sy-eaccdev"
   }
-  instance_state_names = ["running", "stopped"]
-}
-data "aws_instance" "ec2" {
-  for_each = toset([data.aws_instances.ec2list.ids])
-  instance_id       = each.key
-}
 output "aws_ec2_instance_types" {
-  value = [for i in data.aws_instance.ec2 : i.arn]
+  value = [for i in data.aws_instances.ec2list : i.arn]
 }
 
