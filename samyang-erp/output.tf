e32cf6c8dc70fbecec22bb854261a319ad212512
Administrator <hyunseokjung163@gmail.com>
2021-03-24 17:52:06 +0900

    each eky

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -41,6 +41,7 @@ data "aws_ebs_snapshot_ids" "ebs_volumes" {
 data "aws_ebs_snapshot" "ebs_volume" {
   most_recent = true
   for_each    = toset(data.aws_ebs_snapshot_ids.ebs_volumes.ids)
+  snapshot_id = each.key
 }
 
 
