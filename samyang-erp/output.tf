d46a8fb5bde1e4d44605fe9a5d5dd6c30b075909
Administrator <hyunseokjung163@gmail.com>
2021-03-24 11:47:42 +0900

    Filter

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -12,8 +12,9 @@ output "subnet_cidr_blocks" {
   value = [for s in data.aws_subnet.subnets : s.cidr_block]
 }
 data "aws_instances" "ec2list" {
-  instance_tags = {
-    Name = "sy-*"
+  filter {
+    name = "instance.vpc-id"
+    value = [aws.vpc.vpc_id]
   }
   instance_state_names = ["running", "stopped"]
 }
