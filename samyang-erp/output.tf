ca069ce0281821c25b9abe899cfd42b4f170a741
Administrator <hyunseokjung163@gmail.com>
2021-03-24 11:04:25 +0900

    Output 추가

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -11,3 +11,14 @@ data "aws_subnet" "subnets" {
 output "subnet_cidr_blocks" {
   value = [for s in data.aws_subnet.subnets : s.cidr_block]
 }
+data "aws_instances" "ec2list" {
+  instance_state_names = ["running", "stopped"]
+}
+data "aws_instance" "ec2" {
+  for_each = data.ec2list.[*].ids
+  id       = each.value
+}
+output "subnet_cidr_blocks" {
+  value = [for i in data.aws_instance.ec2 : i.id]
+}
+
