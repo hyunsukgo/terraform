323f210988e3e2c4205547b6cdfe5227d77a9a9d
Administrator <hyunseokjung163@gmail.com>
2021-03-24 15:32:36 +0900

    AP SG CIDR로 박혀있는거 제거

--- samyang-erp/aws_security_group.tf
+++ samyang-erp/aws_security_group.tf
@@ -103,7 +103,6 @@ resource "aws_security_group" "allow_from_trust_to_ap" {
     from_port   = 8000
     to_port     = 8999
     protocol    = "tcp"
-    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
     prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
   }
   ingress {
@@ -111,7 +110,6 @@ resource "aws_security_group" "allow_from_trust_to_ap" {
     from_port   = 4300
     to_port     = 4399
     protocol    = "tcp"
-    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
     prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
   }
   ingress {
@@ -119,7 +117,6 @@ resource "aws_security_group" "allow_from_trust_to_ap" {
     from_port   = 30000
     to_port     = 39999
     protocol    = "tcp"
-    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
     prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
   }
   ingress {
@@ -127,7 +124,6 @@ resource "aws_security_group" "allow_from_trust_to_ap" {
     from_port   = 50000
     to_port     = 59999
     protocol    = "tcp"
-    cidr_blocks = ["130.1.0.0/16", "10.200.0.0/16"]
     prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
   }
   egress {
