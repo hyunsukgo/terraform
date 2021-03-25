8f276dcfe729cea3ceefa2b9adc5ed90d056a466
Administrator <hyunseokjung163@gmail.com>
2021-03-25 16:39:26 +0900

    s

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -29,7 +29,7 @@ output "aws_ec2_instance_info" {
 }
 
 output "aws_ec2_ebs_info" {
-  value = formatlist("| %s | %s | %s |", [for name in data.aws_instance.ec2 : name.ebs_block_device.device_name[*]], [for type in data.aws_instance.ec2 : type.ebs_block_device.volume_type],[for size in data.aws_instance.ec2 : size.ebs_block_device.volume_size])
+  value = formatlist("| %s | %s | %s |",[for name in data.aws_instance.ec2 : name.tags.Name], [for type in data.aws_instance.ec2 : type.ebs_block_device.volume_type],[for size in data.aws_instance.ec2 : size.ebs_block_device.volume_size])
 }
 
 
