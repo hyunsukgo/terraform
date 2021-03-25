0130d9c8971b77e33504bd7a8c40786e17816965
Administrator <hyunseokjung163@gmail.com>
2021-03-25 16:25:11 +0900

    r

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -54,5 +54,5 @@ data "aws_ebs_snapshot" "ebs_volume" {
 output "aws_ebs_snapshot_info" {
   #value = data.aws_ebs_snapshot.ebs_volume[*]
   #value = formatlist("%s",[for name in data.aws_ebs_snapshot.ebs_volume : name.id])
-  value = formatlist("%s | %s | %s | %s",[for id in data.aws_ebs_snapshot.ebs_volume : id.id],[for vid in data.aws_ebs_snapshot.ebs_volume : vid.volume_id],[for size in data.aws_ebs_snapshot.ebs_volume : size.volume_size],[for name in data.aws_ebs_snapshot.ebs_volume : name.tags.Name])
+  value = formatlist("| %s | %s | %s G | %s |",[for id in data.aws_ebs_snapshot.ebs_volume : id.id],[for vid in data.aws_ebs_snapshot.ebs_volume : vid.volume_id],[for size in data.aws_ebs_snapshot.ebs_volume : size.volume_size],[for name in data.aws_ebs_snapshot.ebs_volume : name.tags.Name])
 }
