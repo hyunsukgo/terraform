94c33011155975a2fea803e3852649a399453937
Administrator <hyunseokjung163@gmail.com>
2021-03-25 10:35:34 +0900

    false

--- samyang-erp/output.tf
+++ samyang-erp/output.tf
@@ -41,7 +41,7 @@ data "aws_ebs_snapshot_ids" "ebs_volumes" {
 }
 
 data "aws_ebs_snapshot" "ebs_volume" {
-  #most_recent = true
+  most_recent = false
   owners      = ["self"]
   snapshot_ids    = tolist(data.aws_ebs_snapshot_ids.ebs_volumes.ids)
 }
