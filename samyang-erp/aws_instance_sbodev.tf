d675629737d46879a9708ac71b5b973b12e35ce0
Administrator <hyunseokjung163@gmail.com>
2021-03-29 14:15:53 +0900

    이미 인스턴스 생성되어 있음

--- samyang-erp/aws_instance_sbodev.tf
+++ samyang-erp/aws_instance_sbodev.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "sbodev" {
   }
   user_data = file("./scripts/wininst.sh")
   disable_api_termination = "true"
-  security_groups = [aws_security_group.allow_from_trust_to_sbodev.id]
+  #security_groups = [aws_security_group.allow_from_trust_to_sbodev.id]
   subnet_id = aws_subnet.SAPDEV_A.id
 
   tags = {
