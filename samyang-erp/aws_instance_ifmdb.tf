09b98f5c74e8ee4944c77f947c084305bf64bedc
Administrator <hyunseokjung163@gmail.com>
2021-03-30 10:58:13 +0900

    s

--- samyang-erp/aws_instance_ifmdb.tf
+++ samyang-erp/aws_instance_ifmdb.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "ifmdb" {
   }
   user_data = file("./scripts/init.sh")
   disable_api_termination = "true"
-  #security_groups = [aws_security_group.allow_from_trust_to_ifmdb.id]
+  security_groups = [aws_security_group.allow_from_trust_to_ifmdb.id]
   subnet_id = aws_subnet.LEGADB1_A.id
 
   tags = {
