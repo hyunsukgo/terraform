54e0f89467a49199c50a8ab5d031ff6eb39683d6
Administrator <hyunseokjung163@gmail.com>
2021-04-07 16:38:36 +0900

    Module 테스트

--- samyang-erp/module_dev.tf
+++
@@ -1,4 +0,0 @@
-/*module "devenv" {
-  source  = "./dev"
-}
-*/
--- samyang-erp/modules.tf
+++ samyang-erp/modules.tf
@@ -1,5 +1,7 @@
-/*
-module "base" {
-  source = "./base"
+
+module "ec2" {
+  source = "./ec2"
 }
-*/
+module "sg" {
+  source = "./sg"
+}
