9bf56a8912a16318305c42c8caaea24ecff1323a
Administrator <hyunseokjung163@gmail.com>
2021-04-13 15:47:27 +0900

    r

--- samyang-erp/aws_instance_eaccdev.tf
+++ samyang-erp/aws_instance_eaccdev.tf
@@ -1,8 +1,8 @@
 
 resource "aws_instance" "eaccapdev" {
   ami           = "ami-0fc1ba861e48fc890" # ap-northeast-2
-  instance_type = "c5.xlarge"
-
+  instance_type = "m5.xlarge"
+  ebs_optimized = true
   private_ip           = "10.200.60.31"
   key_name             = "samyangerp"
   iam_instance_profile = "ssm"
