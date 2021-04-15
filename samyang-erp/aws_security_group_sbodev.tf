45505ed21e9f4821b514cedbb282afd872252797
Administrator <hyunseokjung163@gmail.com>
2021-04-15 09:12:57 +0900

    8080 추가

--- samyang-erp/aws_security_group_sbodev.tf
+++ samyang-erp/aws_security_group_sbodev.tf
@@ -29,6 +29,14 @@ resource "aws_security_group" "allow_from_trust_to_sbodev" {
   }
 
   ingress {
+    description     = "Application"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
+  }
+
+  ingress {
     description     = "MSSQL Access from On-Prem"
     from_port       = 1433
     to_port         = 1433
