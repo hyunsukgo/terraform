b8987fce7df5dc4336d367b06742a39f5c08f962
Administrator <hyunseokjung163@gmail.com>
2021-04-19 17:28:52 +0900

    패스워드 변경 해봄

--- samyang-erp/aws_db_instance_drawbkdb.tf
+++ samyang-erp/aws_db_instance_drawbkdb.tf
@@ -17,7 +17,7 @@ resource "aws_db_instance" "drawbkdb" {
   name                   = "DRAWBKDB"
   username               = "drawbkdb"
   port                   = 1521
-  password               = var.passwd
+  password               = "!1samyang"
   multi_az               = false
   db_subnet_group_name   = aws_db_subnet_group.drawbkdb.name
   vpc_security_group_ids = [aws_security_group.allow_from_trust_to_drawbkdb.id]
