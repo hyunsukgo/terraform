a567b69e6357d483d42b430d3337dce72ee5f8db
Administrator <hyunseokjung163@gmail.com>
2021-04-27 15:27:55 +0900

    라우팅 테이블 추가

--- samyang-erp/aws_vpc_endpoint.tf
+++ samyang-erp/aws_vpc_endpoint.tf
@@ -40,6 +40,6 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 resource "aws_vpc_endpoint_route_table_association" "s3end_rta" {
-  route_table_id  = aws_route_table.public_route.id
+  route_table_id  = [aws_route_table.public_route.id,aws_route_table.private_route.id]
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
 }
