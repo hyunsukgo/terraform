dda2295bc771dabb8ddcec0854a94de08beada6c
Administrator <hyunseokjung163@gmail.com>
2021-05-04 15:41:56 +0900

    Name Tag 추가

--- samyang-erp/aws_ec2_transit_gateway_vpn_connection.tf
+++ samyang-erp/aws_ec2_transit_gateway_vpn_connection.tf
@@ -3,11 +3,15 @@ resource "aws_vpn_connection" "vpn" {
   transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
   type                = aws_customer_gateway.cgw.type
   static_routes_only  = true
+  tags {
+    Name = "${var.service_name}-VPN"
+  }
 }
 
 resource "aws_vpn_connection" "ksnet-vpn" {
   customer_gateway_id = aws_customer_gateway.ksnet_cgw.id
   transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
   type                = aws_customer_gateway.cgw.type
-  #static_routes_only  = true
+  static_routes_only  = true
+  Name = "${var.ksnet}-VPN"
 }
