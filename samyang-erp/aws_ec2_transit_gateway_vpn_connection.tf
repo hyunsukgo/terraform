49fadc5b8eebc8b9365c3e5fae248c3d5009d922
Administrator <hyunseokjung163@gmail.com>
2021-05-06 10:59:17 +0900

    KSNET 태그 수정

--- samyang-erp/aws_ec2_transit_gateway_vpn_connection.tf
+++ samyang-erp/aws_ec2_transit_gateway_vpn_connection.tf
@@ -14,6 +14,6 @@ resource "aws_vpn_connection" "ksnet-vpn" {
   type                = aws_customer_gateway.cgw.type
   static_routes_only  = true
   tags = {
-    Name = "${var.ksnet}-VPN"
+    Name = "KSNET-VPN"
   }
 }
