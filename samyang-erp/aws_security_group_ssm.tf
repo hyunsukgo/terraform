a8a54dba073d2f4568f4c9e72e4dc015305280d3
Administrator <hyunseokjung163@gmail.com>
2021-05-21 09:35:26 +0900

    SSM Endpoint 설정 추가

---
+++ samyang-erp/aws_security_group_ssm.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_from_trust_to_ssm" {
+  name        = "allow_traffic_ssm"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+  
+  ingress {
+    description     = "SSM Endpoint
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_from_trust_to_ssm"
+  }
+}
