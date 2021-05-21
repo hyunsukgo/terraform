d449dab3272c24624ae85291768dad4c85957fe1
Administrator <hyunseokjung163@gmail.com>
2021-05-21 09:43:59 +0900

    다시

--- samyang-erp/aws_vpc_endpoint.tf
+++ samyang-erp/aws_vpc_endpoint.tf
@@ -52,7 +52,7 @@ resource "aws_vpc_endpoint" "ssm" {
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.ap-northeast-2.ssm"
   vpc_endpoint_type = "Interface"
-  subnet_ids        = [subnet_ids.INTERNAL1_A.id,subnet_ids.INTERNAL2_C.id]
+  subnet_ids        = [aws_subnet.INTERNAL1_A.id,aws_subnet.INTERNAL2_C.id]
   security_group_ids = [
     aws_security_group.allow_from_trust_to_ssm.id,
   ]
