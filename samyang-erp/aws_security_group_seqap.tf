e869c5d6c8b7f44652d0c4727c4ac2b6041816b7
Administrator <hyunseokjung163@gmail.com>
2021-06-01 22:33:46 +0900

    Security Group 신규 생성 인스턴스

---
+++ samyang-erp/aws_security_group_seqap.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_from_trust_to_seqap" {
+  name        = "allow_traffic_seqap"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+  
+  ingress {
+    description     = "seqap Endpoint"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_from_trust_to_seqap"
+  }
+}
---
+++ samyang-erp/aws_security_group_seqdb.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_from_trust_to_seqdb" {
+  name        = "allow_traffic_seqdb"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+  
+  ingress {
+    description     = "seqdb Endpoint"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_from_trust_to_seqdb"
+  }
+}
