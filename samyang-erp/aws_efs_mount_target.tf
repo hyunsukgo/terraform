6e57c1bdeb0374e454de1252b827830568f26df4
Administrator <hyunseokjung163@gmail.com>
2021-06-08 22:54:27 +0900

    이거니?

--- samyang-erp/aws_efs_mount_target.tf
+++ samyang-erp/aws_efs_mount_target.tf
@@ -2,12 +2,12 @@
 resource "aws_efs_mount_target" "efs-mtg_1" {
   file_system_id = aws_efs_file_system.efs.id
   subnet_id      = aws_subnet.INTERNAL1_A.id
-  security_groups = aws_security_group.allow_from_trust_to_efs.id
+  security_groups = [aws_security_group.allow_from_trust_to_efs.id]
 }
  
 
 resource "aws_efs_mount_target" "efs-mtg_2" {
   file_system_id = aws_efs_file_system.efs.id
   subnet_id      = aws_subnet.INTERNAL2_C.id
-  security_groups = aws_security_group.allow_from_trust_to_efs.id
+  security_groups = [aws_security_group.allow_from_trust_to_efs.id]
 }
