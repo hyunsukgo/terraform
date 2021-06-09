1f3184d269e2dfe381e494e8a3381a3105e28165
Administrator <hyunseokjung163@gmail.com>
2021-06-09 11:04:40 +0900

    mount target A 생성

--- samyang-erp/aws_efs_mount_target.tf
+++ samyang-erp/aws_efs_mount_target.tf
@@ -3,6 +3,7 @@ resource "aws_efs_mount_target" "efs-mtg_1" {
   file_system_id = aws_efs_file_system.efs.id
   subnet_id      = aws_subnet.INTERNAL1_A.id
   security_groups = [aws_security_group.allow_from_trust_to_efs.id]
+  ip_address = 10.200.0.206
 }
  
 
