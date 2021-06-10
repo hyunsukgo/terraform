665e621298e9fed0199f5955c63fc5469c64351a
Administrator <hyunseokjung163@gmail.com>
2021-06-10 17:23:45 +0900

    다시

--- samyang-erp/aws_efs_access_point.tf
+++ samyang-erp/aws_efs_access_point.tf
@@ -1,29 +1,29 @@
-resource "aws_efs_access_point" "efs_access_point" {
-  file_system_id = aws_efs_file_system.efs.id
+resource "aws_efs_access_point" "s4h_trans_access_point" {
+  file_system_id = aws_efs_file_system.s4h-trans.id
 
   tags = {
     Name = "${var.service_name}-efs-access-point"
   }
 }
 
-resource "aws_efs_access_point" "s4h_access_point" {
-  file_system_id = aws_efs_file_system.s4h.id
+resource "aws_efs_access_point" "b4h_trans_access_point" {
+  file_system_id = aws_efs_file_system.b4h-trans.id
 
   tags = {
     Name = "${var.service_name}-s4h-access-point"
   }
 }
 
-resource "aws_efs_access_point" "bo_access_point" {
-  file_system_id = aws_efs_file_system.bo.id
+resource "aws_efs_access_point" "po_access_point" {
+  file_system_id = aws_efs_file_system.po-trans.id
 
   tags = {
     Name = "${var.service_name}-bo-access-point"
   }
 }
 
-resource "aws_efs_access_point" "po_access_point" {
-  file_system_id = aws_efs_file_system.po.id
+resource "aws_efs_access_point" "sapcd_access_point" {
+  file_system_id = aws_efs_file_system.sapcd.id
 
   tags = {
     Name = "${var.service_name}-po-access-point"
