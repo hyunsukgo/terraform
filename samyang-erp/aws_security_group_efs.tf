6b7f633db278fe11fb23634abc2c9ce2b4e31502
Administrator <hyunseokjung163@gmail.com>
2021-06-10 09:35:14 +0900

    EFS SG 설정 추가

--- samyang-erp/aws_security_group_efs.tf
+++ samyang-erp/aws_security_group_efs.tf
@@ -9,6 +9,7 @@ resource "aws_security_group" "allow_from_trust_to_efs" {
     to_port         = 2049
     protocol        = "tcp"
     prefix_list_ids = [aws_ec2_managed_prefix_list.trusted.id]
+    security_groups = [aws_security_group.allow_from_trust_to_bw_ap.id,aws_security_group.allow_from_trust_to_bw_db.id,aws_security_group.allow_from_trust_to_seqap.id,aws_security_group.allow_from_trust_to_seqdb.id,aws_security_group.allow_from_trust_to_spqap.id,aws_security_group.allow_from_trust_to_spqdb.id,aws_security_group.allow_from_trust_to_ap.id,aws_security_group.allow_from_trust_to_db.id,aws_security_group.allow_from_trust_to_po.id]
   }
 
   egress {
