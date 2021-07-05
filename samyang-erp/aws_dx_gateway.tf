09ef6e33c6692933e9eeb6d86485f068e777298e
Administrator <hyunseokjung163@gmail.com>
2021-07-05 17:18:27 +0900

    1. DX Gateway 재생성 2. DX Private Gateway 추가

--- samyang-erp/aws_dx_gateway.tf
+++ samyang-erp/aws_dx_gateway.tf
@@ -1,13 +1,14 @@
-resource "aws_dx_gateway" "example" {
+resource "aws_dx_gateway" "dxg" {
   name            = "${var.service_name}-dxg"
   amazon_side_asn = "64512"
 }
 
-resource "aws_dx_private_virtual_interface" "foo" {
+resource "aws_dx_private_virtual_interface" "private_vif" {
   connection_id = "dxcon-ffwkwwak"
 
   name           = "${var.service_name}-vif"
   vlan           = 4094
   address_family = "ipv4"
   bgp_asn        = 65352
+  dx_gateway_id  = aws_dx_gateway.dxg.id
 }
