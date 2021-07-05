6545edbb4e71d2c06c835b2f8c340ed1d1515388
Administrator <hyunseokjung163@gmail.com>
2021-07-05 17:12:05 +0900

    Private VIF

--- samyang-erp/aws_dx_gateway.tf
+++ samyang-erp/aws_dx_gateway.tf
@@ -2,3 +2,12 @@ resource "aws_dx_gateway" "example" {
   name            = "${var.service_name}-dxg"
   amazon_side_asn = "64512"
 }
+
+resource "aws_dx_private_virtual_interface" "foo" {
+  connection_id = "dxcon-ffwkwwak"
+
+  name           = "${var.service_name}-vif"
+  vlan           = 4094
+  address_family = "ipv4"
+  bgp_asn        = 65352
+}
