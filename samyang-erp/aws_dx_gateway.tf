a54e9488cebaf3f9511a0504a15fe9a882c8adc1
Administrator <hyunseokjung163@gmail.com>
2021-07-06 10:35:52 +0900

    DX Assoc생성

--- samyang-erp/aws_dx_gateway.tf
+++ samyang-erp/aws_dx_gateway.tf
@@ -2,13 +2,19 @@ resource "aws_dx_gateway" "dxg" {
   name            = "${var.service_name}-dxg"
   amazon_side_asn = "64512"
 }
-
+/*
 resource "aws_dx_private_virtual_interface" "private_vif" {
   connection_id = "dxcon-ffwkwwak"
 
   name           = "${var.service_name}-vif"
-  vlan           = 4094
+  vlan           = 117
   address_family = "ipv4"
   bgp_asn        = 65352
   dx_gateway_id  = aws_dx_gateway.dxg.id
+}
+*/
+
+resource "aws_dx_gateway_association" "dxg_assoc" {
+  dx_gateway_id         = aws_dx_gateway.dxg.id
+  associated_gateway_id = aws_ec2_transit_gateway.tgw.id
 }
