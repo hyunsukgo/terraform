ed55f95330170ba06957a01637227bb2143b0347
Administrator <hyunseokjung163@gmail.com>
2021-07-07 10:46:57 +0900

    다시 TGW로 바꿔봄 DX gateway로 되려나?

--- samyang-erp/aws_dx_gateway.tf
+++ samyang-erp/aws_dx_gateway.tf
@@ -16,9 +16,10 @@ resource "aws_dx_private_virtual_interface" "private_vif" {
 
 resource "aws_dx_gateway_association" "dxg_assoc" {
   dx_gateway_id         = aws_dx_gateway.dxg.id
-  associated_gateway_id = aws_vpn_gateway.vpn_gw.id
+  #associated_gateway_id = aws_vpn_gateway.vpn_gw.id
+  associated_gateway_id = aws_ec2_transit_gateway.tgw.id
 }
-
+/*
 resource "aws_vpn_gateway" "vpn_gw" {
   vpc_id = aws_vpc.vpc.id
 
@@ -26,3 +27,4 @@ resource "aws_vpn_gateway" "vpn_gw" {
     Name = "DX Gateway"
   }
 }
+*/
