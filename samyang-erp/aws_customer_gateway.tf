51dcdfcd5cb14f43034a808da7f44ad7ef2916d9
Administrator <hyunseokjung163@gmail.com>
2021-07-09 20:13:05 +0900

    다시

--- samyang-erp/aws_customer_gateway.tf
+++ samyang-erp/aws_customer_gateway.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "aws_customer_gateway" "cgw" {
   bgp_asn    = 65000
   ip_address = var.cgw
@@ -8,7 +8,7 @@ resource "aws_customer_gateway" "cgw" {
     Name = "${var.service_name}-customer-gateway"
   }
 }
-*/
+
 resource "aws_customer_gateway" "ksnet_cgw" {
   bgp_asn    = 65000
   ip_address = var.ksnet-cgw
--- samyang-erp/aws_ec2_transit_gateway_vpn_attachment.tf
+++ samyang-erp/aws_ec2_transit_gateway_vpn_attachment.tf
@@ -1,8 +1,8 @@
-data "aws_ec2_transit_gateway_vpn_attachment" "tgw_attachment_vpn" {
+/*data "aws_ec2_transit_gateway_vpn_attachment" "tgw_attachment_vpn" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   vpn_connection_id  = aws_vpn_connection.vpn.id
 }
-
+*/
 data "aws_ec2_transit_gateway_vpn_attachment" "tgw_attachment_ksnet-vpn" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   vpn_connection_id  = aws_vpn_connection.ksnet-vpn.id
