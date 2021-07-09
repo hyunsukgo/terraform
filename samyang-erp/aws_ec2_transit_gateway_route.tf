b298f8c675d59e7a075a3ef977e5fe0bf0d51e5a
Administrator <hyunseokjung163@gmail.com>
2021-07-09 20:15:22 +0900

    제거

--- samyang-erp/aws_ec2_transit_gateway_route.tf
+++ samyang-erp/aws_ec2_transit_gateway_route.tf
@@ -1,8 +1,9 @@
-resource "aws_ec2_transit_gateway_route" "vpn_route" {
+/*resource "aws_ec2_transit_gateway_route" "vpn_route" {
   destination_cidr_block         = var.onprem
   transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_vpn_attachment.tgw_attachment_vpn.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
 }
+*/
 resource "aws_ec2_transit_gateway_route" "ksnet_vpn_route" {
   destination_cidr_block         = var.ksnet
   transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_vpn_attachment.tgw_attachment_ksnet-vpn.id
