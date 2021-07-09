90631c1f5d95d0e397194a2c4340672a65344748
Administrator <hyunseokjung163@gmail.com>
2021-07-09 19:11:32 +0900

    Route Table 변경

--- samyang-erp/aws_public_route_table.tf
+++ samyang-erp/aws_public_route_table.tf
@@ -10,12 +10,12 @@ resource "aws_route_table" "public_route" {
     cidr_block                = "10.100.0.0/16"
     vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
   }
-
+/*
   route {
     cidr_block         = "130.1.0.0/16"
     transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   }
-
+*/
   route {
     cidr_block         = var.ksnet
     transit_gateway_id = aws_ec2_transit_gateway.tgw.id
