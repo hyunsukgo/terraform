7563e482148384af50f5937375cdad73306108a6
Administrator <hyunseokjung163@gmail.com>
2021-07-19 14:02:31 +0900

    prefix로

--- samyang-erp/aws_s3_bucket_installation.tf
+++ samyang-erp/aws_s3_bucket_installation.tf
@@ -9,6 +9,16 @@ resource "aws_s3_bucket" "installationbucket" {
 }
 
 resource "aws_s3_bucket" "launchwizard" {
+  bucket        = "${var.service_name}-launchwizard-bucket"
+  force_destroy = true
+
+  tags = {
+    Name        = "${var.service_name}-launchwizard-bucket"
+    Environment = "Prd"
+  }
+}
+
+resource "aws_s3_bucket" "lw" {
   bucket        = "launchwizard-${var.service_name}-bucket"
   force_destroy = true
 
