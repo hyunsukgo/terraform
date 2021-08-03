6fe548f3812314f5a1d5ba059a8d982b6845c76b
Administrator <hyunseokjung163@gmail.com>
2021-08-03 16:22:03 +0900

    NLB 추가

--- samyang-erp/aws_sap_nlb.tf
+++ samyang-erp/aws_sap_nlb.tf
@@ -29,3 +29,28 @@ resource "aws_lb_target_group" "sap_hana" {
   target_type = "ip"
   vpc_id      = aws_vpc.vpc.id
 }
+
+resource "aws_lb" "sep_nlb" {
+  name               = "samyang-sep-lb"
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = [aws_subnet.SAPAP1_A.id, aws_subnet.SAPAP2_C.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.sep_nlb.arn
+  port              = "3610"
+  protocol          = "TCP"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ascs_msg_server.arn
+  }
+}
