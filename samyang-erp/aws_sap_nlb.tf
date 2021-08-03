bce55a6e40dd14685ba88c7d395179566d38bf57
Administrator <hyunseokjung163@gmail.com>
2021-08-03 16:14:49 +0900

    ㅡㅡ

--- samyang-erp/aws_sap_nlb.tf
+++ samyang-erp/aws_sap_nlb.tf
@@ -29,29 +29,3 @@ resource "aws_lb_target_group" "sap_hana" {
   target_type = "ip"
   vpc_id      = aws_vpc.vpc.id
 }
-
-resource "aws_lb_target_group_attachment" "ascs_msg_server" {
-  target_group_arn = aws_lb_target_group.ascs_msg_server.arn
-  target_id        = "10.150.220.10"
-  port             = 3610
-}
-
-resource "aws_lb_target_group_attachment" "sap_start" {
-  target_group_arn = aws_lb_target_group.sap_start.arn
-  target_id        = "10.150.110.10"
-  port             = 50013
-}
-
-
-resource "aws_lb_target_group_attachment" "jdbc_port" {
-  target_group_arn = aws_lb_target_group.jdbc_port.arn
-  target_id        = "10.150.110.10"
-  port             = 30015
-}
-
-
-resource "aws_lb_target_group_attachment" "sap_hana" {
-  target_group_arn = aws_lb_target_group.sap_hana.arn
-  target_id        = "10.150.110.10"
-  port             = 30013
-}
