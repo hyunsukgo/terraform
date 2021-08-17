32ffd23623e383931e0d4ac8977ac279926027da
Administrator <hyunseokjung163@gmail.com>
2021-08-17 10:00:53 +0900

    SPQ DB 인스턴스 디스크 추가

--- samyang-erp/aws_instance_spqdb.tf
+++ samyang-erp/aws_instance_spqdb.tf
@@ -85,7 +85,7 @@ resource "aws_volume_attachment" "spqdb_att_3" {
 
 resource "aws_ebs_volume" "spqdb_add_3" {
   availability_zone = "${var.region}a"
-  size              = 64
+  size              = 128
   iops              = 3000
   type              = "gp3"
   throughput        = 125
@@ -95,6 +95,185 @@ resource "aws_ebs_volume" "spqdb_add_3" {
     Snapshot  = "true"
     Name      = "sy-spo-spqdb"
     Partition = "/swap"
+    cz-ext1   = "sy-spqdb"
+  }
+}
+
+
+resource "aws_volume_attachment" "spqdb_att_4" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.spqdb_add_4.id
+  instance_id = aws_instance.spqdb.id
+}
+
+resource "aws_ebs_volume" "spqdb_add_4" {
+  availability_zone = "${var.region}a"
+  size              = 128
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 500
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+  tags = {
+    Snapshot  = "true"
+    Name      = "sy-spo-spqdb"
+    Partition = "/hana/shared/SPD"
+    cz-ext1   = "sy-spqdb"
+  }
+}
+
+
+resource "aws_volume_attachment" "spqdb_att_5" {
+  device_name = "/dev/sdg"
+  volume_id   = aws_ebs_volume.spqdb_add_5.id
+  instance_id = aws_instance.spqdb.id
+}
+
+resource "aws_ebs_volume" "spqdb_add_5" {
+  availability_zone = "${var.region}a"
+  size              = 256
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 500
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+  tags = {
+    Snapshot  = "true"
+    Name      = "sy-spo-spqdb"
+    Partition = "/hana/data/SPD"
+    cz-ext1   = "sy-spqdb"
+  }
+}
+
+resource "aws_volume_attachment" "spqdb_att_6" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.spqdb_add_6.id
+  instance_id = aws_instance.spqdb.id
+}
+
+resource "aws_ebs_volume" "spqdb_add_6" {
+  availability_zone = "${var.region}a"
+  size              = 64
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 500
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+  tags = {
+    Snapshot  = "true"
+    Name      = "sy-spo-spqdb"
+    Partition = "/hana/log/SPD"
+    cz-ext1   = "sy-spqdb"
+  }
+}
+
+resource "aws_volume_attachment" "spqdb_att_7" {
+  device_name = "/dev/sdi"
+  volume_id   = aws_ebs_volume.spqdb_add_7.id
+  instance_id = aws_instance.spqdb.id
+}
+
+resource "aws_ebs_volume" "spqdb_add_7" {
+  availability_zone = "${var.region}a"
+  size              = 20
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 125
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+  tags = {
+    Snapshot  = "true"
+    Name      = "sy-spo-spqdb"
+    Partition = "/usr/sap/SPD"
+    cz-ext1   = "sy-spqdb"
+  }
+}
+
+resource "aws_volume_attachment" "spqdb_att_8" {
+  device_name = "/dev/sdj"
+  volume_id   = aws_ebs_volume.spqdb_add_8.id
+  instance_id = aws_instance.spqdb.id
+}
+
+resource "aws_ebs_volume" "spqdb_add_8" {
+  availability_zone = "${var.region}a"
+  size              = 128
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 500
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+  tags = {
+    Snapshot  = "true"
+    Name      = "sy-spo-spqdb"
+    Partition = "/hana/shared/SPQ"
+    cz-ext1   = "sy-spqdb"
+  }
+}
+
+
+resource "aws_volume_attachment" "spqdb_att_9" {
+  device_name = "/dev/sdk"
+  volume_id   = aws_ebs_volume.spqdb_add_9.id
+  instance_id = aws_instance.spqdb.id
+}
+
+resource "aws_ebs_volume" "spqdb_add_9" {
+  availability_zone = "${var.region}a"
+  size              = 256
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 500
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+  tags = {
+    Snapshot  = "true"
+    Name      = "sy-spo-spqdb"
+    Partition = "/hana/data/SPQ"
+    cz-ext1   = "sy-spqdb"
+  }
+}
+
+resource "aws_volume_attachment" "spqdb_att_10" {
+  device_name = "/dev/sdl"
+  volume_id   = aws_ebs_volume.spqdb_add_10.id
+  instance_id = aws_instance.spqdb.id
+}
+
+resource "aws_ebs_volume" "spqdb_add_10" {
+  availability_zone = "${var.region}a"
+  size              = 64
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 500
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+  tags = {
+    Snapshot  = "true"
+    Name      = "sy-spo-spqdb"
+    Partition = "/hana/log/SPQ"
+    cz-ext1   = "sy-spqdb"
+  }
+}
+
+resource "aws_volume_attachment" "spqdb_att_11" {
+  device_name = "/dev/sdm"
+  volume_id   = aws_ebs_volume.spqdb_add_11.id
+  instance_id = aws_instance.spqdb.id
+}
+
+resource "aws_ebs_volume" "spqdb_add_11" {
+  availability_zone = "${var.region}a"
+  size              = 20
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 125
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+  tags = {
+    Snapshot  = "true"
+    Name      = "sy-spo-spqdb"
+    Partition = "/usr/sap/SPQ"
     cz-ext1   = "sy-spqdb"
   }
 }
