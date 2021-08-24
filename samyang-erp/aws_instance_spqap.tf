4d97a5715bcab047dab5c36ccfdcb8b3c9b9599f
Administrator <hyunseokjung163@gmail.com>
2021-08-24 10:24:11 +0900

    다시
    Signed-off-by: Administrator <hyunseokjung163@gmail.com>

--- samyang-erp/aws_instance_spqap.tf
+++ samyang-erp/aws_instance_spqap.tf
@@ -35,13 +35,13 @@ resource "aws_instance" "spqap" {
     cz-ext1     = "sy-spqdb"
   }
 }
-
+/*
 resource "aws_volume_attachment" "spqap_att_1" {
   device_name = "/dev/sdb"
   volume_id   = aws_ebs_volume.spqap_add_1.id
   instance_id = aws_instance.spqap.id
 }
-/*
+
 resource "aws_ebs_volume" "spqap_add_1" {
   availability_zone = "${var.region}a"
   size              = 100
