ae4eca729f7d8f0f3c26213946f38d1480cafddd
Administrator <hyunseokjung163@gmail.com>
2021-08-24 13:23:50 +0900

    리스너 추가#3
    Signed-off-by: Administrator <hyunseokjung163@gmail.com>

--- samyang-erp/aws_elb.tf
+++ samyang-erp/aws_elb.tf
@@ -132,6 +132,6 @@ resource "aws_lb_listener" "po-web-listener" {
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.po-web-tga.arn
+    target_group_arn = aws_lb_target_group.po-web.arn
   }
 }
