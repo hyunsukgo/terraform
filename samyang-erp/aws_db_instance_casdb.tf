2ca9d1d4aa5e7e21ebb6cd389d8f49dfe7426be3
Administrator <hyunseokjung163@gmail.com>
2021-08-30 12:23:22 +0900

    CAS DB 추가

---
+++ samyang-erp/aws_db_instance_casdb.tf
@@ -0,0 +1,43 @@
+
+resource "aws_db_instance" "casdb" {
+  identifier = "casdb"
+
+  engine         = "oracle-se2"
+  engine_version = "19.0.0.0.ru-2021-04.rur-2021-04.r1"
+  #family               = "oracle-se-18.0" # DB parameter group
+  #major_engine_version = "12.1"           # DB option group
+  instance_class = "db.m5.4xlarge"
+  license_model  = "license-included"
+
+  allocated_storage     = 100
+  max_allocated_storage = 2048
+  storage_encrypted     = true
+
+  # Make sure that database name is capitalized, otherwise RDS will try to recreate RDS instance every time
+  name                   = "CASDB"
+  username               = "casdb"
+  port                   = 1521
+  password               = "!1samyang"
+  multi_az               = false
+  db_subnet_group_name   = aws_db_subnet_group.casdb.name
+  vpc_security_group_ids = [aws_security_group.allow_from_trust_to_casdb.id]
+
+  backup_retention_period = 0
+  skip_final_snapshot     = true
+  deletion_protection     = true
+  tags = {
+    Snapshot = "Yes"
+    cz-ext1  = "sy-casdb"
+  }
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
+}
+
+resource "aws_db_subnet_group" "casdb" {
+  name       = "casdb-db-subnets"
+  subnet_ids = [aws_subnet.LEGARDS_A.id, aws_subnet.LEGARDS_C.id]
+
+  tags = {
+    Name = "casdb DB subnet group"
+  }
+}
