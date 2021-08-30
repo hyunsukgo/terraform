d6e60b4294ee478c87b3be5685bed8052bb9d9bd
Administrator <hyunseokjung163@gmail.com>
2021-08-30 11:02:03 +0900

    잘못 수정한거 다시

--- samyang-erp/aws_efs_access_point.tf
+++ samyang-erp/aws_efs_access_point.tf
@@ -13,7 +13,7 @@ resource "aws_efs_access_point" "b4h_trans_access_point" {
     Name = "${var.service_name}-s4h-access-point"
   }
 }
-/*
+
 resource "aws_efs_access_point" "po_access_point" {
   file_system_id = aws_efs_file_system.po-trans.id
 
@@ -21,7 +21,7 @@ resource "aws_efs_access_point" "po_access_point" {
     Name = "${var.service_name}-bo-access-point"
   }
 }
-*/
+
 resource "aws_efs_access_point" "sapcd_access_point" {
   file_system_id = aws_efs_file_system.sapcd.id
 
@@ -29,7 +29,7 @@ resource "aws_efs_access_point" "sapcd_access_point" {
     Name = "${var.service_name}-po-access-point"
   }
 }
-
+/*
 resource "aws_efs_access_point" "po_mnt_qas_access_point" {
   file_system_id = aws_efs_file_system.po-mnt-qas.id
 
@@ -37,3 +37,4 @@ resource "aws_efs_access_point" "po_mnt_qas_access_point" {
     Name = "${var.service_name}-po-mnt-qas-access-point"
   }
 }
+*/
