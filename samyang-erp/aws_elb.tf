daec8bfcaa61998561145798ba0c77885c6021ca
Administrator <hyunseokjung163@gmail.com>
2021-09-03 10:28:19 +0900

    수정

--- samyang-erp/aws_elb.tf
+++ samyang-erp/aws_elb.tf
@@ -51,7 +51,7 @@ resource "aws_lb_target_group" "po-int" {
 }
 
 resource "aws_lb_target_group" "po" {
-  name     = "po-int-tg"
+  name     = "po-tg"
   port     = 8120
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
@@ -71,7 +71,7 @@ resource "aws_lb_target_group_attachment" "po-tga" {
 
 resource "aws_lb_listener_rule" "po" {
   listener_arn = aws_lb_listener.reportlb.arn
-  priority     = 100
+  priority     = 102
 
   action {
     type             = "forward"
