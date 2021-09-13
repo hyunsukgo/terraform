1fe2f6175c060bdb5c6ab2792b4349c5a5947772
Administrator <hyunseokjung163@gmail.com>
2021-09-13 15:30:11 +0900

    SAPROUTER

--- samyang-erp/aws_security_group_saprouter.tf
+++ samyang-erp/aws_security_group_saprouter.tf
@@ -35,4 +35,44 @@ resource "aws_security_group" "allow_from_trust_to_saprouter" {
   tags = {
     Name = "allow_from_trust_to_saprouter"
   }
+}
+
+resource "aws_security_group" "SAPROUTER" {
+  name        = "SAPROUTER"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description     = "SSH"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = "3.36.176.77/32"
+  }
+  ingress {
+    description = "SapRouter Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks     = "3.36.176.77/32"
+  }
+
+  ingress {
+    description = "SapRouter Access"
+    from_port   = 3200
+    to_port     = 3299
+    protocol    = "tcp"
+    cidr_blocks     = "3.36.176.77/32"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SAPROUTER"
+  }
 } 
