25423f5cb2690c611e29304672662bc6d054f833
Administrator <hyunseokjung163@gmail.com>
2021-09-30 14:24:23 +0900

    EFS SG 함께 적용

--- samyang-erp/aws_efs_mount_target.tf
+++ samyang-erp/aws_efs_mount_target.tf
@@ -2,14 +2,14 @@
 resource "aws_efs_mount_target" "s4h-trans-a" {
   file_system_id  = aws_efs_file_system.s4h-trans.id
   subnet_id       = aws_subnet.NAS1_A.id
-  security_groups = [aws_security_group.allow_from_trust_to_s4h_trans_efs.id]
+  security_groups = [aws_security_group.S4H_DEV_QAS_AP.id ,aws_security_group.S4H_PRD_AP.id]
   ip_address      = "10.200.0.132"
 }
 
 resource "aws_efs_mount_target" "s4h-trans-c" {
   file_system_id  = aws_efs_file_system.s4h-trans.id
   subnet_id       = aws_subnet.NAS2_C.id
-  security_groups = [aws_security_group.allow_from_trust_to_s4h_trans_efs.id]
+  security_groups = [aws_security_group.S4H_DEV_QAS_AP.id ,aws_security_group.S4H_PRD_AP.id]
   ip_address      = "10.200.0.172"
 }
 
@@ -17,7 +17,7 @@ resource "aws_efs_mount_target" "s4h-trans-c" {
 resource "aws_efs_mount_target" "b4h-trans-a" {
   file_system_id  = aws_efs_file_system.b4h-trans.id
   subnet_id       = aws_subnet.NAS1_A.id
-  security_groups = [aws_security_group.allow_from_trust_to_b4h_trans_efs.id]
+  security_groups = [aws_security_group.B4H_DEV_AP.id ,aws_security_group.B4H_PRD_AP.id]
   ip_address      = "10.200.0.134"
 }
 
@@ -25,21 +25,21 @@ resource "aws_efs_mount_target" "b4h-trans-a" {
 resource "aws_efs_mount_target" "b4h-trans-c" {
   file_system_id  = aws_efs_file_system.b4h-trans.id
   subnet_id       = aws_subnet.NAS2_C.id
-  security_groups = [aws_security_group.allow_from_trust_to_b4h_trans_efs.id]
+  security_groups = [aws_security_group.B4H_DEV_AP.id ,aws_security_group.B4H_PRD_AP.id]
   ip_address      = "10.200.0.174"
 }
 
 resource "aws_efs_mount_target" "po-trans-a" {
   file_system_id  = aws_efs_file_system.po-trans.id
   subnet_id       = aws_subnet.NAS1_A.id
-  security_groups = [aws_security_group.allow_from_trust_to_po_trans_efs.id]
+  security_groups = [aws_security_group.PO_DEV_QAS_AP.id ,aws_security_group.PO_PRD_AP.id]
   ip_address      = "10.200.0.136"
 }
 
 resource "aws_efs_mount_target" "po-trans-c" {
   file_system_id  = aws_efs_file_system.po-trans.id
   subnet_id       = aws_subnet.NAS2_C.id
-  security_groups = [aws_security_group.allow_from_trust_to_po_trans_efs.id]
+  security_groups = [aws_security_group.PO_DEV_QAS_AP.id ,aws_security_group.PO_PRD_AP.id]
   ip_address      = "10.200.0.176"
 }
 
