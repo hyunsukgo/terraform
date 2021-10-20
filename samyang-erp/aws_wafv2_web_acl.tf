2ef3771d2c6e3701ba8a890a0b7c0042711b5e64
Administrator <hyunseokjung163@gmail.com>
2021-10-20 14:22:58 +0900

    Processor 삭제?

--- samyang-erp/aws_wafv2_web_acl.tf
+++ samyang-erp/aws_wafv2_web_acl.tf
@@ -26,11 +26,6 @@ resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
   extended_s3_configuration {
     role_arn   = aws_iam_role.firehose_role.arn
     bucket_arn = aws_s3_bucket.waf.arn
-
-    processing_configuration {
-      enabled = "true"
-
-    }
   }
 }
 
