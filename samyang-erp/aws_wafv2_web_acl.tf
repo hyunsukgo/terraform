864130f42cd3733a5d98c09aa7652409dcae25b4
Administrator <hyunseokjung163@gmail.com>
2021-10-20 14:56:04 +0900

    dma?

--- samyang-erp/aws_wafv2_web_acl.tf
+++ samyang-erp/aws_wafv2_web_acl.tf
@@ -51,7 +51,7 @@ EOF
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "extended_s3_stream" {
-  log_destination_configs = aws_kinesis_firehose_delivery_stream.extended_s3_stream.arn
+  log_destination_configs = "arn:aws:firehose:ap-northeast-2:969470234861:deliverystream/terraform-kinesis-firehose-extended-s3-waf-stream"
   resource_arn            = aws_wafv2_web_acl.web_acl.arn
   redacted_fields {
     single_header {
